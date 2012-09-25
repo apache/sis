56d5bd91221e580a03029a2674ba027ccf788a82
Martin Desruisseaux <desruisseaux@apache.org>
2012-09-25 08:28:53 +0000

    Added documentation.
    git-svn-id: https://svn.apache.org/repos/asf/sis/trunk@1389753 13f79535-47bb-0310-9956-ffa450edef68

--- sis-utility/src/main/java/org/apache/sis/resources/package-info.java
+++ sis-utility/src/main/java/org/apache/sis/resources/package-info.java
@@ -16,16 +16,36 @@
  */
 
 /**
- * Localized resources for SIS.
+ * Localized resources for SIS. While anyone could use the resources provided in this package,
+ * those resources are primarily for internal SIS usage and may change in any future version.
  *
- * <STRONG>Do not use!</STRONG>
+ * <p>Apache SIS resources are provided in binary files having the "{@code .utf}" extension.
+ * The resource keys are numeric constants declared in the {@code Keys} static inner classes.
+ * Values are strings which may optionally have slots for one or more parameters, identified
+ * by the "<code>{</code><var>n</var><code>}</code>" characters sequences where <var>n</var>
+ * is the parameter number (first parameter is "<code>{0}</code>").
+ * If, and only if, a string value has slots for at least one parameter, then:</p>
  *
- * This package is for internal use by SIS only. Classes in this package
- * may change in incompatible ways in any future version without notice.
+ * <ul>
+ *   <li>the key name ends with the {@code '_'} character followed by the expected number of parameters;</li>
+ *   <li>the value string will be compliant with the {@link java.text.MessageFormat} syntax.</li>
+ * </ul>
+ *
+ * <p>Apache SIS developers can add resources by editing the {@code *.properties} file
+ * in the source code directory, then run the localized resources compiler provided in the
+ * <code><a href="{@website}/sis-build-helper/index.html">sis-build-helper</a></code> module.
+ * Developers shall <strong>not</strong> apply the {@code MessageFormat} rules for using quotes,
+ * since the resources compiler will apply itself the <cite>doubled single quotes</cite> when
+ * necessary. This avoid the unfortunate confusion documented in the warning section of
+ * {@link java.text.MessageFormat} javadoc.</p>
  *
  * @author  Martin Desruisseaux (Geomatys)
  * @since   0.3 (derived from geotk-1.2)
  * @version 0.3
  * @module
+ *
+ * @see java.util.ResourceBundle
+ * @see java.text.MessageFormat
+ * @see <a href="{@website}/sis-build-helper/index.html"><code>sis-build-helper</code> module</a>
  */
 package org.apache.sis.resources;
