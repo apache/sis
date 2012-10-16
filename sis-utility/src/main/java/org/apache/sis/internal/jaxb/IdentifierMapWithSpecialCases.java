481904f34dcb5db9c491613fd8ef028c9569775c
Martin Desruisseaux <desruisseaux@apache.org>
2012-10-16 10:31:44 +0000

    Added IdentifierMap implementation.
    git-svn-id: https://svn.apache.org/repos/asf/sis/branches/JDK7@1398723 13f79535-47bb-0310-9956-ffa450edef68

---
+++ sis-utility/src/main/java/org/apache/sis/internal/jaxb/IdentifierMapWithSpecialCases.java
@@ -0,0 +1,205 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.sis.internal.jaxb;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Collection;
+import java.util.Objects;
+
+import org.opengis.metadata.Identifier;
+import org.opengis.metadata.citation.Citation;
+
+import org.apache.sis.xml.IdentifierSpace;
+import org.apache.sis.xml.XLink;
+
+
+/**
+ * A map of identifiers which handles some identifiers in a special way.
+ * The identifiers for the following authorities are handled in a special way.
+ * See usages of {@link #isSpecialCase(Citation)} for spotting the code where
+ * a special handling is applied.
+ * <p>
+ * <ul>
+ *   <li>{@link IdentifierSpace#HREF}, handled as a shortcut to {@link XLink#getHRef()}.</li>
+ * </ul>
+ *
+ * @author  Martin Desruisseaux (Geomatys)
+ * @since   0.3 (derived from geotk-3.19)
+ * @version 0.3
+ * @module
+ */
+final class IdentifierMapWithSpecialCases extends IdentifierMapAdapter {
+    /**
+     * For cross-version compatibility.
+     */
+    private static final long serialVersionUID = 5139573827448780289L;
+
+    /**
+     * Creates a new map which will be a view over the given identifiers.
+     *
+     * @param identifiers The identifiers to wrap in a map view.
+     */
+    public IdentifierMapWithSpecialCases(final Collection<Identifier> identifiers) {
+        super(identifiers);
+    }
+
+    /**
+     * If the given authority is a special case, returns its {@link NonMarshalledAuthority}
+     * integer enum. Otherwise returns -1. See javadoc for more information about special cases.
+     */
+    private static int specialCase(final Object authority) {
+        if (authority == IdentifierSpace.HREF) return NonMarshalledAuthority.HREF;
+        // A future Apache SIS version may add more special cases here.
+        return -1;
+    }
+
+    /**
+     * Extracts the {@code xlink:href} value from the {@link XLink} if presents. This method does
+     * not test if an explicit {@code xlink:href} identifier exists - this check must be done by
+     * the caller <strong>before</strong> to invoke this method, by invoking one of the getter
+     * methods defined in the {@link IdentifierMapAdapter} super-class.
+     */
+    private String getHRef() {
+        final XLink link = super.getSpecialized(IdentifierSpace.XLINK);
+        if (link != null) {
+            final URI href = link.getHRef();
+            if (href != null) {
+                return href.toString();
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Sets the {@code xlink:href} value, which may be null. If an explicit {@code xlink:href}
+     * identifier exists, it is removed before to set the new {@code href} in the {@link XLink}
+     * object.
+     */
+    private URI setHRef(final URI href) {
+        super.putSpecialized(IdentifierSpace.HREF, null);
+        XLink link = super.getSpecialized(IdentifierSpace.XLINK);
+        if (link != null) {
+            final URI old = link.getHRef();
+            link.setHRef(href);
+            return old;
+        }
+        if (href != null) {
+            link = new XLink();
+            link.setHRef(href);
+            super.putSpecialized(IdentifierSpace.XLINK, link);
+        }
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean containsValue(final Object code) {
+        return super.containsValue(code) || Objects.equals(code, getHRef());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean containsKey(final Object authority) {
+        if (super.containsKey(authority)) {
+            return true;
+        }
+        switch (specialCase(authority)) {
+            case NonMarshalledAuthority.HREF: {
+                final XLink link = super.getSpecialized(IdentifierSpace.XLINK);
+                return (link != null) && (link.getHRef() != null);
+            }
+        }
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T> T getSpecialized(final IdentifierSpace<T> authority) {
+        T value = super.getSpecialized(authority);
+        if (value == null) {
+            switch (specialCase(authority)) {
+                case NonMarshalledAuthority.HREF: {
+                    final XLink link = super.getSpecialized(IdentifierSpace.XLINK);
+                    if (link != null) {
+                        value = (T) link.getHRef();
+                    }
+                    break;
+                }
+            }
+        }
+        return value;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String get(final Object authority) {
+        String value = super.get(authority);
+        if (value == null) {
+            switch (specialCase(authority)) {
+                case NonMarshalledAuthority.HREF: {
+                    value = getHRef();
+                    break;
+                }
+            }
+        }
+        return value;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String put(final Citation authority, final String code) throws UnsupportedOperationException {
+        switch (specialCase(authority)) {
+            case NonMarshalledAuthority.HREF: {
+                try {
+                    final URI old = setHRef((code != null) ? new URI(code) : null);
+                    return (old != null) ? old.toString() : null;
+                } catch (URISyntaxException e) {
+                    // Do not log the exception, since it will be
+                    // reported by super.put(Citation, String).
+                }
+                break;
+            }
+        }
+        return super.put(authority, code);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T> T putSpecialized(final IdentifierSpace<T> authority, final T value) throws UnsupportedOperationException {
+        switch (specialCase(authority)) {
+            case NonMarshalledAuthority.HREF: {
+                return (T) setHRef((URI) value);
+            }
+        }
+        return super.putSpecialized(authority, value);
+    }
+}
---
+++ sis-utility/src/main/java/org/apache/sis/internal/jaxb/SpecializedIdentifier.java
@@ -0,0 +1,218 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.sis.internal.jaxb;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.UUID;
+import java.io.Serializable;
+import org.opengis.metadata.Identifier;
+import org.opengis.metadata.citation.Citation;
+import org.apache.sis.xml.XLink;
+import org.apache.sis.xml.IdentifierMap;
+import org.apache.sis.xml.IdentifierSpace;
+import org.apache.sis.util.logging.Logging;
+import org.apache.sis.internal.util.Citations;
+
+// Related to JDK7
+import java.util.Objects;
+
+
+/**
+ * Wraps a {@link XLink}, {@link URI} or {@link UUID} as an identifier in the {@link IdentifierMap}.
+ * The {@linkplain #authority} is typically an instance of {@link NonMarshalledAuthority}. The value
+ * is an object of a type constrained by the authority.
+ *
+ * @param  <T> The value type, typically {@link XLink}, {@link UUID} or {@link String}.
+ *
+ * @author  Martin Desruisseaux (Geomatys)
+ * @since   0.3 (derived from geotk-3.19)
+ * @version 0.3
+ * @module
+ */
+public final class SpecializedIdentifier<T> implements Identifier, Serializable {
+    /**
+     * For cross-version compatibility.
+     */
+    private static final long serialVersionUID = 1673231050676950993L;
+
+    /**
+     * The authority, typically as a {@link NonMarshalledAuthority) instance.
+     * Null value is not recommended, but this {@code SpecializedIdentifier}
+     * is tolerant to such cases.
+     *
+     * @see #getAuthority()
+     */
+    private final IdentifierSpace<T> authority;
+
+    /**
+     * The identifier value. The identifier {@linkplain #getCode() code} will be the
+     * {@linkplain Object#toString() string representation} of this value, if non-null.
+     *
+     * <p>This value is set at construction time, but may be modified later by
+     * {@link IdentifierMapAdapter#putSpecialized(IdentifierSpace, Object)}.</p>
+     *
+     * @see #getValue()
+     * @see #getCode()
+     */
+    T value;
+
+    /**
+     * Creates a new adapter for the given authority and identifier value.
+     *
+     * @param authority The identifier authority.
+     * @param value The identifier value, or {@code null} if not yet defined.
+     */
+    public SpecializedIdentifier(final IdentifierSpace<T> authority, final T value) {
+        this.authority = authority;
+        this.value = value;
+    }
+
+    /**
+     * Creates an identifier from a text value. This method creates an instance of
+     * {@code SpecializedIdentifier} if the given authority is one of the "special"
+     * authorities declared in the {@link IdentifierSpace} interface. Otherwise a
+     * plain {@link IdentifierMapEntry} is created.
+     *
+     * @param authority The authority, typically as one of the {@link IdentifierSpace} constants.
+     * @param code The identifier code to parse.
+     *
+     * @see IdentifierMapAdapter#put(Citation, String)
+     */
+    static Identifier parse(final Citation authority, final String code) {
+        if (authority instanceof NonMarshalledAuthority) {
+            switch (((NonMarshalledAuthority) authority).ordinal) {
+                case NonMarshalledAuthority.ID: {
+                    return new SpecializedIdentifier<>(IdentifierSpace.ID, code);
+                }
+                case NonMarshalledAuthority.UUID: {
+                    try {
+                        return new SpecializedIdentifier<>(IdentifierSpace.UUID, UUID.fromString(code));
+                    } catch (IllegalArgumentException e) {
+                        parseFailure(e);
+                        break;
+                    }
+                }
+                case NonMarshalledAuthority.HREF: {
+                    final URI href;
+                    try {
+                        href = new URI(code);
+                    } catch (URISyntaxException e) {
+                        parseFailure(e);
+                        break;
+                    }
+                    return new SpecializedIdentifier<>(IdentifierSpace.HREF, href);
+                }
+                case NonMarshalledAuthority.XLINK: {
+                    final URI href;
+                    try {
+                        href = new URI(code);
+                    } catch (URISyntaxException e) {
+                        parseFailure(e);
+                        break;
+                    }
+                    final XLink xlink = new XLink();
+                    xlink.setHRef(href);
+                    return new SpecializedIdentifier<>(IdentifierSpace.XLINK, xlink);
+                }
+            }
+        }
+        return new IdentifierMapEntry(authority, code);
+    }
+
+    /**
+     * Invoked by {@link #parse(Citation,String)} when a string can not be parsed.
+     * This is considered a non-fatal error, because the parse method can fallback
+     * on the generic {@link IdentifierMapEntry} in such cases.
+     */
+    private static void parseFailure(final Exception e) {
+        // IdentifierMap.put(Citation,String) is the public facade.
+        Logging.recoverableException(IdentifierMap.class, "put", e);
+    }
+
+    /**
+     * Returns the authority specified at construction time.
+     */
+    @Override
+    public Citation getAuthority() {
+        return authority;
+    }
+
+    /**
+     * Returns the identifier value. This is the {@linkplain #getCode() code} expressed as
+     * an object more specialized than {@link String}.
+     *
+     * @return The identifier value, or {@code null} if none.
+     */
+    public T getValue() {
+        return value;
+    }
+
+    /**
+     * Returns a string representation of the {@linkplain #getValue() identifier value},
+     * or {@code null} if none.
+     */
+    @Override
+    public String getCode() {
+        final T value = this.value;
+        return (value != null) ? value.toString() : null;
+    }
+
+    /**
+     * Returns a hash code value for this identifier.
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(value) + 31 * Objects.hashCode(authority);
+    }
+
+    /**
+     * Compares this identifier with the given object for equality.
+     *
+     * @param other The object to compare with this identifier for equality.
+     */
+    @Override
+    public boolean equals(final Object other) {
+        if (other instanceof SpecializedIdentifier<?>) {
+            final SpecializedIdentifier<?> that = (SpecializedIdentifier<?>) other;
+            return Objects.equals(authority, that.authority) &&
+                   Objects.equals(value, that.value);
+        }
+        return false;
+    }
+
+    /**
+     * Returns a string representation of this identifier.
+     */
+    @Override
+    public String toString() {
+        final StringBuilder buffer = new StringBuilder("Identifier[");
+        format(buffer, authority, getCode());
+        return buffer.append(']').toString();
+    }
+
+    /**
+     * Formats the given (authority, code) par value in the given buffer.
+     */
+    static void format(final StringBuilder buffer, final Citation authority, final String code) {
+        buffer.append(Citations.getIdentifier(authority)).append('=');
+        final boolean quote = (code != null) && (code.indexOf('[') < 0);
+        if (quote) buffer.append('“');
+        buffer.append(code);
+        if (quote) buffer.append('”');
+    }
+}
