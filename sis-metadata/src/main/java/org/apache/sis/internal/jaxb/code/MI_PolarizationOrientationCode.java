4ee2914c3919bf663ff6abcf1066c9bf5131d635
Martin Desruisseaux <desruisseaux@apache.org>
2012-12-07 08:52:17 +0000

    Added JAXB adapters for ISO 19115[-2] CodeLists.
    git-svn-id: https://svn.apache.org/repos/asf/sis/branches/JDK7@1418222 13f79535-47bb-0310-9956-ffa450edef68

---
+++ sis-metadata/src/main/java/org/apache/sis/internal/jaxb/code/MI_PolarizationOrientationCode.java
@@ -0,0 +1,95 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.sis.internal.jaxb.code;
+
+import org.apache.sis.internal.jaxb.gmd.CodeListAdapter;
+import org.apache.sis.internal.jaxb.gmd.CodeListProxy;
+import javax.xml.bind.annotation.XmlElement;
+import org.opengis.metadata.content.PolarizationOrientation;
+import org.apache.sis.xml.Namespaces;
+
+
+/**
+ * JAXB adapter for {@link PolarizationOrientation}, in order to integrate the value in an element respecting
+ * the ISO-19139 standard. See package documentation for more information about the handling
+ * of {@code CodeList} in ISO-19139.
+ *
+ * @author  Cédric Briançon (Geomatys)
+ * @author  Guilhem Legal (Geomatys)
+ * @since   0.3 (derived from geotk-3.02)
+ * @version 0.3
+ * @module
+ */
+public final class MI_PolarizationOrientationCode
+        extends CodeListAdapter<MI_PolarizationOrientationCode, PolarizationOrientation>
+{
+    /**
+     * Ensures that the adapted code list class is loaded.
+     */
+    static {
+        ensureClassLoaded(PolarizationOrientation.class);
+    }
+
+    /**
+     * Empty constructor for JAXB only.
+     */
+    public MI_PolarizationOrientationCode() {
+    }
+
+    /**
+     * Creates a new adapter for the given proxy.
+     */
+    private MI_PolarizationOrientationCode(final CodeListProxy proxy) {
+        super(proxy);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected MI_PolarizationOrientationCode wrap(CodeListProxy proxy) {
+        return new MI_PolarizationOrientationCode(proxy);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected Class<PolarizationOrientation> getCodeListClass() {
+        return PolarizationOrientation.class;
+    }
+
+    /**
+     * Invoked by JAXB on marshalling.
+     *
+     * @return The value to be marshalled.
+     */
+    @Override
+    @XmlElement(name = "MI_PolarizationOrientationCode", namespace = Namespaces.GMI)
+    public CodeListProxy getElement() {
+        return proxy;
+    }
+
+    /**
+     * Invoked by JAXB on unmarshalling.
+     *
+     * @param proxy The unmarshalled value.
+     */
+    public void setElement(final CodeListProxy proxy) {
+        this.proxy = proxy;
+    }
+}
