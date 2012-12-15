7b2e27bfb1d8437903d5712002bc6327e71fb4df
Martin Desruisseaux <desruisseaux@apache.org>
2012-12-15 13:11:19 +0000

    Added tests for DirectPosition implementations.
    git-svn-id: https://svn.apache.org/repos/asf/sis/branches/JDK7@1422244 13f79535-47bb-0310-9956-ffa450edef68

---
+++ sis-referencing/src/test/java/org/apache/sis/geometry/DirectPosition1DTest.java
@@ -0,0 +1,82 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.sis.geometry;
+
+import org.opengis.geometry.DirectPosition;
+import org.apache.sis.test.DependsOn;
+import org.apache.sis.test.TestCase;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+/**
+ * Tests the {@link DirectPosition1D} class.
+ *
+ * @author  Martin Desruisseaux (IRD, Geomatys)
+ * @since   0.3 (derived from geotk-2.4)
+ * @version 0.3
+ * @module
+ */
+@DependsOn(GeneralDirectPositionTest.class)
+public final strictfp class DirectPosition1DTest extends TestCase {
+    /**
+     * Tests the {@link DirectPosition1D#toString()} method.
+     */
+    @Test
+    public void testWktFormatting() {
+        assertEquals("POINT(8.5)", new DirectPosition1D(8.5).toString());
+    }
+
+    /**
+     * Tests the {@link DirectPosition1D#DirectPosition2D(String)} constructor.
+     */
+    @Test
+    public void testWktParsing() {
+        assertEquals("POINT(8)", new DirectPosition1D("POINT(8)").toString());
+    }
+
+    /**
+     * Tests {@link DirectPosition2D#equals(Object)} method between different implementations.
+     * The purpose of this test is also to run the assertion in the direct position implementations.
+     */
+    @Test
+    public void testEquals() {
+        assertTrue(DirectPosition1D     .class.desiredAssertionStatus());
+        assertTrue(GeneralDirectPosition.class.desiredAssertionStatus());
+
+        DirectPosition p1 = new DirectPosition1D     (48.543261561072285);
+        DirectPosition p2 = new GeneralDirectPosition(48.543261561072285);
+        assertTrue(p1.equals(p2));
+        assertTrue(p2.equals(p1));
+
+        p1.setOrdinate(0, p1.getOrdinate(0) + 1);
+        assertFalse(p1.equals(p2));
+        assertFalse(p2.equals(p1));
+    }
+
+    /**
+     * Tests {@link DirectPosition1D#clone()}.
+     */
+    @Test
+    public void testClone() {
+        final DirectPosition1D p1 = new DirectPosition1D(20);
+        final DirectPosition1D p2 = p1.clone();
+        assertEquals("Expected the same CRS and ordinates.", p1, p2);
+        assertEquals("Expected the same ordinates.", 20.0, p2.ordinate, 0.0);
+    }
+}
