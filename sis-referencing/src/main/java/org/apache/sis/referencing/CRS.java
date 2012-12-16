ffefec5689c48b33e35d65ab82b5bdac6ab54cbb
Martin Desruisseaux <desruisseaux@apache.org>
2012-12-16 08:24:44 +0000

    Ported the ArrayEnvelope class.
    git-svn-id: https://svn.apache.org/repos/asf/sis/branches/JDK7@1422481 13f79535-47bb-0310-9956-ffa450edef68

---
+++ sis-referencing/src/main/java/org/apache/sis/referencing/CRS.java
@@ -0,0 +1,59 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.sis.referencing;
+
+import org.opengis.util.FactoryException;
+import org.opengis.referencing.NoSuchAuthorityCodeException;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import org.apache.sis.util.Static;
+
+import static org.apache.sis.util.ArgumentChecks.ensureNonNull;
+
+
+/**
+ * Static methods working on {@linkplain CoordinateReferenceSystem Coordinate Reference Systems}.
+ *
+ * @author Martin Desruisseaux (IRD, Geomatys)
+ * @since   0.3 (derived from geotk-2.1)
+ * @version 0.3
+ * @module
+ */
+public final class CRS extends Static {
+    /**
+     * Do not allow instantiation of this class.
+     */
+    private CRS() {
+    }
+
+    /**
+     * Returns a Coordinate Reference System from the given authority code.
+     *
+     * @todo This method is not yet implemented. It will be provided after the EPSG-backed
+     *       authority factory has been ported to Apache SIS.
+     *
+     * @param  code The authority code.
+     * @return The Coordinate Reference System for the given authority code.
+     * @throws NoSuchAuthorityCodeException If there is no known CRS associated to the given code.
+     * @throws FactoryException if the CRS creation failed for an other reason.
+     */
+    public static CoordinateReferenceSystem forCode(final String code)
+            throws NoSuchAuthorityCodeException, FactoryException
+    {
+        ensureNonNull("code", code);
+        throw new UnsupportedOperationException("Not yet implemented.");
+    }
+}
