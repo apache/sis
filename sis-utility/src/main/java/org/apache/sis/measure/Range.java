d9be0b5eaf2e78129feef6a7030aba04e3e7f1b7
Martin Desruisseaux <desruisseaux@apache.org>
2013-02-11 16:20:38 +0000

    Acknowledge the source of the <T extends Number & Comparable<? super T>> signature. Note that there is no other way to express the condition "type T extends both Number and Comparable". So legally, I think that we are okay since anyone wanting to parameterize NumberRange can only obtain that exact signature; there is no flexibility here (except for the 'T' variable name).  Even the type order is enforced by the Java language.
    git-svn-id: https://svn.apache.org/repos/asf/sis/branches/JDK7@1444845 13f79535-47bb-0310-9956-ffa450edef68

--- sis-utility/src/main/java/org/apache/sis/measure/Range.java
+++ sis-utility/src/main/java/org/apache/sis/measure/Range.java
@@ -58,6 +58,7 @@ import java.util.Objects;
  *
  * @author  Joe White
  * @author  Martin Desruisseaux (Geomatys)
+ * @author  Jody Garnett (for parameterized type suggestion)
  * @since   0.3
  * @version 0.3
  * @module
