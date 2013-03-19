9e0d336ad7e80bf7af90307db5e1c2d144a259e7
Martin Desruisseaux <desruisseaux@apache.org>
2013-03-19 16:15:17 +0000

    Added an interface which is going to be needed by metadata.
    git-svn-id: https://svn.apache.org/repos/asf/sis/branches/JDK7@1458377 13f79535-47bb-0310-9956-ffa450edef68

---
+++ sis-utility/src/main/java/org/apache/sis/util/Deprecable.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.sis.util;
+
+
+/**
+ * Interface of classes for which deprecated instances may exist. Deprecated instances exist in some
+ * {@linkplain org.opengis.referencing.AuthorityFactory authority factories} like the EPSG database.
+ * Some examples of deprecated instances are:
+ *
+ * <ul>
+ *   <li>An {@link org.apache.sis.referencing.AbstractIdentifiedObject} (typically a CRS)
+ *       which has been built from a deprecated EPSG code.</li>
+ *   <li>A {@link org.apache.sis.referencing.NamedIdentifier} containing the legacy name
+ *       of an object which has been renamed.</li>
+ * </ul>
+ *
+ * @author Martin Desruisseaux (Geomatys)
+ * @since   0.3 (derived from geotk-3.20)
+ * @version 0.3
+ * @module
+ */
+public interface Deprecable {
+    /**
+     * Returns {@code true} if this instance is deprecated.
+     *
+     * @return {@code true} if this instance is deprecated.
+     */
+    boolean isDeprecated();
+}
