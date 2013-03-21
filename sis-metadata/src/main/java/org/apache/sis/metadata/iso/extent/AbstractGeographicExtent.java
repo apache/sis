0e72987034e487a2944a573c649d6745065eaf65
Martin Desruisseaux <desruisseaux@apache.org>
2013-03-21 08:29:35 +0000

    Removed the "This package contains documentation from OGC specifications" notice from header, and put it in the NOTICE file instead.
    git-svn-id: https://svn.apache.org/repos/asf/sis/branches/JDK7@1459189 13f79535-47bb-0310-9956-ffa450edef68

--- sis-metadata/src/main/java/org/apache/sis/metadata/iso/extent/AbstractGeographicExtent.java
+++ sis-metadata/src/main/java/org/apache/sis/metadata/iso/extent/AbstractGeographicExtent.java
@@ -13,9 +13,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * This package contains documentation from OGC specifications.
- * Open Geospatial Consortium's work is fully acknowledged here.
  */
 package org.apache.sis.metadata.iso.extent;
 
