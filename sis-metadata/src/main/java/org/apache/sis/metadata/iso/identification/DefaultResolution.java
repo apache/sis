4742b627ea3b2e32686ec05ed8900baff21fcfe2
Martin Desruisseaux <desruisseaux@apache.org>
2013-03-21 11:47:50 +0000

    Added the identification package.
    git-svn-id: https://svn.apache.org/repos/asf/sis/branches/JDK7@1459242 13f79535-47bb-0310-9956-ffa450edef68

--- sis-metadata/src/main/java/org/apache/sis/metadata/iso/identification/DefaultResolution.java
+++ sis-metadata/src/main/java/org/apache/sis/metadata/iso/identification/DefaultResolution.java
@@ -16,23 +16,121 @@
  */
 package org.apache.sis.metadata.iso.identification;
 
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import org.opengis.metadata.identification.RepresentativeFraction;
 import org.opengis.metadata.identification.Resolution;
+// import org.apache.sis.internal.jaxb.gco.GO_Distance; // TODO
 import org.apache.sis.metadata.iso.ISOMetadata;
+import org.apache.sis.measure.ValueRange;
 
+
+/**
+ * Level of detail expressed as a scale factor or a ground distance.
+ *
+ * @author  Martin Desruisseaux (IRD, Geomatys)
+ * @author  Touraïvane (IRD)
+ * @author  Cédric Briançon (Geomatys)
+ * @since   0.3 (derived from geotk-2.1)
+ * @version 0.3
+ * @module
+ */
+@XmlType(name = "MD_Resolution_Type", propOrder = {
+    "equivalentScale",
+    "distance"
+})
+@XmlRootElement(name = "MD_Resolution")
 public class DefaultResolution extends ISOMetadata implements Resolution {
+    /**
+     * Serial number for compatibility with different versions.
+     */
+    private static final long serialVersionUID = -4644465057871958482L;
 
+    /**
+     * Level of detail expressed as the scale of a comparable hardcopy map or chart.
+     * This value should be between 0 and 1.
+     * Only one of {@linkplain #getEquivalentScale() equivalent scale} and
+     * {@linkplain #getDistance() ground sample distance} may be provided.
+     */
     private RepresentativeFraction equivalentScale;
 
+    /**
+     * Ground sample distance.
+     * Only one of {@linkplain #getEquivalentScale() equivalent scale} and
+     * {@linkplain #getDistance() ground sample distance} may be provided.
+     */
     private Double distance;
 
+    /**
+     * Constructs an initially empty resolution.
+     */
+    public DefaultResolution() {
+    }
+
+    /**
+     * Returns a SIS metadata implementation with the same values than the given arbitrary
+     * implementation. If the given object is {@code null}, then this method returns {@code null}.
+     * Otherwise if the given object is already a SIS implementation, then the given object is
+     * returned unchanged. Otherwise a new SIS implementation is created and initialized to the
+     * property values of the given object, using a <cite>shallow</cite> copy operation
+     * (i.e. properties are not cloned).
+     *
+     * @param  object The object to get as a SIS implementation, or {@code null} if none.
+     * @return A SIS implementation containing the values of the given object (may be the
+     *         given object itself), or {@code null} if the argument was null.
+     */
+    public static DefaultResolution castOrCopy(final Resolution object) {
+        if (object == null || object instanceof DefaultResolution) {
+            return (DefaultResolution) object;
+        }
+        final DefaultResolution copy = new DefaultResolution();
+        copy.shallowCopy(object);
+        return copy;
+    }
+
+    /**
+     * Returns the level of detail expressed as the scale of a comparable hardcopy map or chart.
+     * Only one of {@linkplain #getEquivalentScale() equivalent scale} and
+     * {@linkplain #getDistance() ground sample distance} may be provided.
+     */
     @Override
-    public synchronized RepresentativeFraction getEquivalentScale() {
+    @XmlElement(name = "equivalentScale")
+    public synchronized RepresentativeFraction getEquivalentScale()  {
         return equivalentScale;
     }
 
+    /**
+     * Sets the level of detail expressed as the scale of a comparable hardcopy map or chart.
+     *
+     * @param newValue The new equivalent scale.
+     */
+    public synchronized void setEquivalentScale(final RepresentativeFraction newValue) {
+        checkWritePermission();
+        equivalentScale = newValue;
+    }
+
+    /**
+     * Returns the ground sample distance.
+     * Only one of {@linkplain #getEquivalentScale equivalent scale} and
+     * {@linkplain #getDistance ground sample distance} may be provided.
+     */
     @Override
+    @ValueRange(minimum=0, isMinIncluded=false)
+//    @XmlJavaTypeAdapter(GO_Distance.class) // TODO
+    @XmlElement(name = "distance")
     public synchronized Double getDistance() {
         return distance;
+    }
+
+    /**
+     * Sets the ground sample distance.
+     *
+     * @param newValue The new distance.
+     */
+    public synchronized void setDistance(final Double newValue) {
+        checkWritePermission();
+        distance = newValue;
     }
 }
---
+++ sis-metadata/src/main/java/org/apache/sis/metadata/iso/identification/package-info.java
@@ -0,0 +1,84 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * {@linkplain org.apache.sis.metadata.iso.identification.AbstractIdentification Identification} implementation.
+ * An explanation for this package is provided in the {@linkplain org.opengis.metadata.content OpenGIS® javadoc}.
+ * The remaining discussion on this page is specific to the SIS implementation.
+ *
+ * {@section Overview}
+ * For a global overview of metadata in SIS, see the
+ * <a href="{@docRoot}/../sis-metadata/index.html">Metadata page on the project web site</a>.
+ *
+ * @author  Martin Desruisseaux (IRD, Geomatys)
+ * @author  Touraïvane (IRD)
+ * @author  Cédric Briançon (Geomatys)
+ * @since   0.3 (derived from geotk-2.1)
+ * @version 0.3
+ * @module
+ */
+@XmlSchema(elementFormDefault = XmlNsForm.QUALIFIED, namespace = Namespaces.GMD, xmlns = {
+    @XmlNs(prefix = "gmd", namespaceURI = Namespaces.GMD),
+    @XmlNs(prefix = "gco", namespaceURI = Namespaces.GCO),
+    @XmlNs(prefix = "xsi", namespaceURI = Namespaces.XSI)
+})
+@XmlAccessorType(XmlAccessType.NONE)
+@XmlJavaTypeAdapters({
+    @XmlJavaTypeAdapter(CI_Citation.class),
+    @XmlJavaTypeAdapter(CI_ResponsibleParty.class),
+    @XmlJavaTypeAdapter(DS_AssociationTypeCode.class),
+    @XmlJavaTypeAdapter(DS_InitiativeTypeCode.class),
+    @XmlJavaTypeAdapter(EX_Extent.class),
+    @XmlJavaTypeAdapter(MD_AggregateInformation.class),
+    @XmlJavaTypeAdapter(MD_BrowseGraphic.class),
+    @XmlJavaTypeAdapter(MD_CharacterSetCode.class),
+    @XmlJavaTypeAdapter(MD_Constraints.class),
+    @XmlJavaTypeAdapter(MD_Format.class),
+    @XmlJavaTypeAdapter(MD_Identifier.class),
+    @XmlJavaTypeAdapter(MD_Keywords.class),
+    @XmlJavaTypeAdapter(MD_KeywordTypeCode.class),
+    @XmlJavaTypeAdapter(MD_MaintenanceInformation.class),
+    @XmlJavaTypeAdapter(MD_ProgressCode.class),
+    @XmlJavaTypeAdapter(MD_RepresentativeFraction.class),
+    @XmlJavaTypeAdapter(MD_Resolution.class),
+    @XmlJavaTypeAdapter(MD_SpatialRepresentationTypeCode.class),
+    @XmlJavaTypeAdapter(MD_TopicCategoryCode.class),
+    @XmlJavaTypeAdapter(MD_Usage.class),
+
+    // Java types, primitive types and basic OGC types handling
+    @XmlJavaTypeAdapter(URIAdapter.class),
+    @XmlJavaTypeAdapter(LocaleAdapter.class),
+    @XmlJavaTypeAdapter(StringAdapter.class),
+    @XmlJavaTypeAdapter(InternationalStringAdapter.class),
+//  @XmlJavaTypeAdapter(GO_DateTime.class), TODO
+    @XmlJavaTypeAdapter(GO_Decimal.class),        @XmlJavaTypeAdapter(type=double.class, value=GO_Decimal.class),
+    @XmlJavaTypeAdapter(GO_Integer.AsLong.class), @XmlJavaTypeAdapter(type=long.class,   value=GO_Integer.AsLong.class)
+})
+package org.apache.sis.metadata.iso.identification;
+
+import javax.xml.bind.annotation.XmlNs;
+import javax.xml.bind.annotation.XmlNsForm;
+import javax.xml.bind.annotation.XmlSchema;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapters;
+import org.apache.sis.xml.Namespaces;
+import org.apache.sis.internal.jaxb.gco.*;
+import org.apache.sis.internal.jaxb.gmd.*;
+import org.apache.sis.internal.jaxb.code.*;
+import org.apache.sis.internal.jaxb.metadata.*;
