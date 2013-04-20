a1a2f65bccebb213fa85f47daffdf4f6a1379d3d
Martin Desruisseaux <desruisseaux@apache.org>
2013-04-20 16:56:20 +0000

    Removed synchronization in CheckedArrayList and CheckedHashSet, following removal of synchronization in metadata class (SIS-85). Those checked collections were used only by metadata; their synchronization behavior shall be consistent with the metadata one.
    git-svn-id: https://svn.apache.org/repos/asf/sis/branches/JDK7@1470193 13f79535-47bb-0310-9956-ffa450edef68

--- sis-utility/src/main/java/org/apache/sis/internal/util/CheckedHashSet.java
+++ sis-utility/src/main/java/org/apache/sis/internal/util/CheckedHashSet.java
@@ -22,7 +22,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedHashSet;
 import java.util.NoSuchElementException;
-import net.jcip.annotations.ThreadSafe;
 import org.apache.sis.util.Decorator;
 import org.apache.sis.util.resources.Errors;
 import org.apache.sis.util.collection.CheckedContainer;
@@ -31,28 +30,17 @@ import static org.apache.sis.util.ArgumentChecks.ensureNonNull;
 
 
 /**
- * A {@linkplain Collections#checkedSet(Set, Class) checked} and
- * {@linkplain Collections#synchronizedSet(Set) synchronized} {@link LinkedHashSet}.
+ * A {@linkplain Collections#checkedSet(Set, Class) checked} {@link LinkedHashSet}.
  * The type checks are performed at run-time in addition to the compile-time checks.
  *
  * <p>Using this class is similar to wrapping a {@link LinkedHashSet} using the methods provided
  * in the standard {@link Collections} class, except for the following advantages:</p>
  *
  * <ul>
- *   <li>Avoid the two levels of indirection (for type check and synchronization).</li>
+ *   <li>Avoid one level of indirection.</li>
  *   <li>Checks for write permission.</li>
- *   <li>Overrideable methods for controlling the synchronization lock,
- *       type checks and write permission checks.</li>
+ *   <li>Overrideable methods for controlling the type checks and write permission checks.</li>
  * </ul>
- *
- * The synchronization is provided mostly in order to prevent damages
- * to the set in case of concurrent access. It does <strong>not</strong> prevent
- * {@link java.util.ConcurrentModificationException} to be thrown during iterations,
- * unless the whole iteration is synchronized on this set {@linkplain #getLock() lock}.
- * For real concurrency, see the {@link java.util.concurrent} package instead.
- *
- * {@note The above is the reason why the name of this class emphases the <cite>checked</cite>
- *        aspect rather than the <cite>synchronized</cite> aspect of the set.}
  *
  * @param <E> The type of elements in the set.
  *
@@ -62,9 +50,7 @@ import static org.apache.sis.util.ArgumentChecks.ensureNonNull;
  * @module
  *
  * @see Collections#checkedSet(Set, Class)
- * @see Collections#synchronizedSet(Set)
  */
-@ThreadSafe
 public class CheckedHashSet<E> extends LinkedHashSet<E> implements CheckedContainer<E>, Cloneable {
     /**
      * Serial version UID for compatibility with different versions.
@@ -113,11 +99,6 @@ public class CheckedHashSet<E> extends LinkedHashSet<E> implements CheckedContai
      * to the type specified at construction time. Subclasses can override this method
      * if they need to perform additional checks.
      *
-     * {@section Synchronization}
-     * This method is invoked <em>before</em> to get the synchronization {@linkplain #getLock() lock}.
-     * This is different than the {@link #checkWritePermission()} method, which is invoked inside the
-     * synchronized block.
-     *
      * @param  element the object to check, or {@code null}.
      * @throws IllegalArgumentException if the specified element can not be added to this set.
      */
@@ -141,45 +122,24 @@ public class CheckedHashSet<E> extends LinkedHashSet<E> implements CheckedContai
     }
 
     /**
-     * Checks if changes in this set are allowed. This method is automatically invoked
-     * after this set got the {@linkplain #getLock() lock} and before any operation that
-     * may change the content. If the write operation is allowed, then this method shall
-     * returns normally. Otherwise an {@link UnsupportedOperationException} is thrown.
+     * Checks if changes in this set are allowed. This method is automatically invoked before any
+     * operation that may change the content. If the write operation is allowed, then this method
+     * shall returns normally. Otherwise an {@link UnsupportedOperationException} is thrown.
      *
-     * <p>The default implementation does nothing significant (see below), thus allowing this set to
-     * be modified. Subclasses can override this method if they want to control write permissions.</p>
-     *
-     * {@note Actually the current implementation contains an <code>assert</code> statement
-     *        ensuring that the thread holds the lock. This is an implementation details that may
-     *        change in any future version of the SIS library. Nevertheless methods that override
-     *        this one are encouraged to invoke <code>super.checkWritePermission()</code>.}
+     * <p>The default implementation does nothing, thus allowing this set to be modified.
+     * Subclasses can override this method if they want to control write permissions.</p>
      *
      * @throws UnsupportedOperationException if this set is unmodifiable.
      */
     protected void checkWritePermission() throws UnsupportedOperationException {
-        assert Thread.holdsLock(getLock());
     }
 
     /**
-     * Returns the synchronization lock. The default implementation returns {@code this}.
-     *
-     * {@section Note for subclass implementors}
-     * Subclasses that override this method must be careful to update the lock reference
-     * (if needed) when this set is {@linkplain #clone() cloned}.
-     *
-     * @return The synchronization lock.
-     */
-    protected Object getLock() {
-        return this;
-    }
-
-    /**
-     * A synchronized iterator with a check for write permission prior element removal.
+     * An iterator with a check for write permission prior element removal.
      * This class wraps the iterator provided by {@link LinkedHashSet#iterator()}.
      *
      * @see CheckedHashSet#iterator()
      */
-    @ThreadSafe
     @Decorator(Iterator.class)
     private final class Iter implements Iterator<E> {
         /** The {@link LinkedHashSet} iterator. */
@@ -193,26 +153,20 @@ public class CheckedHashSet<E> extends LinkedHashSet<E> implements CheckedContai
         /** Returns {@code true} if there is more elements in the iteration. */
         @Override
         public boolean hasNext() {
-            synchronized (getLock()) {
-                return iterator.hasNext();
-            }
+            return iterator.hasNext();
         }
 
         /** Returns the next element in the iteration. */
         @Override
         public E next() throws NoSuchElementException {
-            synchronized (getLock()) {
-                return iterator.next();
-            }
+            return iterator.next();
         }
 
         /** Removes the previous element if the enclosing {@link CheckedHashSet} allows write operations. */
         @Override
         public void remove() throws UnsupportedOperationException {
-            synchronized (getLock()) {
-                checkWritePermission();
-                iterator.remove();
-            }
+            checkWritePermission();
+            iterator.remove();
         }
     }
 
@@ -223,39 +177,7 @@ public class CheckedHashSet<E> extends LinkedHashSet<E> implements CheckedContai
      */
     @Override
     public Iterator<E> iterator() {
-        synchronized (getLock()) {
-            return new Iter(super.iterator());
-        }
-    }
-
-    /**
-     * Returns the number of elements in this set.
-     */
-    @Override
-    public int size() {
-        synchronized (getLock()) {
-            return super.size();
-        }
-    }
-
-    /**
-     * Returns {@code true} if this set contains no elements.
-     */
-    @Override
-    public boolean isEmpty() {
-        synchronized (getLock()) {
-            return super.isEmpty();
-        }
-    }
-
-    /**
-     * Returns {@code true} if this set contains the specified element.
-     */
-    @Override
-    public boolean contains(final Object o) {
-        synchronized (getLock()) {
-            return super.contains(o);
-        }
+        return new Iter(super.iterator());
     }
 
     /**
@@ -271,10 +193,8 @@ public class CheckedHashSet<E> extends LinkedHashSet<E> implements CheckedContai
             throws IllegalArgumentException, UnsupportedOperationException
     {
         ensureValid(element);
-        synchronized (getLock()) {
-            checkWritePermission();
-            return super.add(element);
-        }
+        checkWritePermission();
+        return super.add(element);
     }
 
     /**
@@ -290,10 +210,8 @@ public class CheckedHashSet<E> extends LinkedHashSet<E> implements CheckedContai
             throws IllegalArgumentException, UnsupportedOperationException
     {
         ensureValidCollection(collection);
-        synchronized (getLock()) {
-            checkWritePermission();
-            return super.addAll(collection);
-        }
+        checkWritePermission();
+        return super.addAll(collection);
     }
 
     /**
@@ -303,10 +221,8 @@ public class CheckedHashSet<E> extends LinkedHashSet<E> implements CheckedContai
      */
     @Override
     public boolean remove(Object o) throws UnsupportedOperationException {
-        synchronized (getLock()) {
-            checkWritePermission();
-            return super.remove(o);
-        }
+        checkWritePermission();
+        return super.remove(o);
     }
 
     /**
@@ -316,10 +232,8 @@ public class CheckedHashSet<E> extends LinkedHashSet<E> implements CheckedContai
      */
     @Override
     public boolean removeAll(Collection<?> c) throws UnsupportedOperationException {
-        synchronized (getLock()) {
-            checkWritePermission();
-            return super.removeAll(c);
-        }
+        checkWritePermission();
+        return super.removeAll(c);
     }
 
     /**
@@ -329,10 +243,8 @@ public class CheckedHashSet<E> extends LinkedHashSet<E> implements CheckedContai
      */
     @Override
     public boolean retainAll(Collection<?> c) throws UnsupportedOperationException {
-        synchronized (getLock()) {
-            checkWritePermission();
-            return super.retainAll(c);
-        }
+        checkWritePermission();
+        return super.retainAll(c);
     }
 
     /**
@@ -342,74 +254,7 @@ public class CheckedHashSet<E> extends LinkedHashSet<E> implements CheckedContai
      */
     @Override
     public void clear() throws UnsupportedOperationException {
-        synchronized (getLock()) {
-            checkWritePermission();
-            super.clear();
-        }
-    }
-
-    /**
-     * Returns an array containing all of the elements in this set.
-     */
-    @Override
-    public Object[] toArray() {
-        synchronized (getLock()) {
-            return super.toArray();
-        }
-    }
-
-    /**
-     * Returns an array containing all of the elements in this set.
-     *
-     * @param <T> The type of array elements.
-     */
-    @Override
-    public <T> T[] toArray(T[] a) {
-        synchronized (getLock()) {
-            return super.toArray(a);
-        }
-    }
-
-    /**
-     * Returns a string representation of this set.
-     */
-    @Override
-    public String toString() {
-        synchronized (getLock()) {
-            return super.toString();
-        }
-    }
-
-    /**
-     * Compares the specified object with this set for equality.
-     */
-    @Override
-    public boolean equals(Object o) {
-        synchronized (getLock()) {
-            return super.equals(o);
-        }
-    }
-
-    /**
-     * Returns the hash code value for this set.
-     */
-    @Override
-    public int hashCode() {
-        synchronized (getLock()) {
-            return super.hashCode();
-        }
-    }
-
-    /**
-     * Returns a shallow copy of this set.
-     *
-     * @return A shallow copy of this set.
-     */
-    @Override
-    @SuppressWarnings("unchecked")
-    public CheckedHashSet<E> clone() {
-        synchronized (getLock()) {
-            return (CheckedHashSet<E>) super.clone();
-        }
+        checkWritePermission();
+        super.clear();
     }
 }
