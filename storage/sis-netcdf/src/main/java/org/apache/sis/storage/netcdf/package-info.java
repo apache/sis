ad5f7e39ff63f459800349a5c2063bab4a8a2d13
Martin Desruisseaux <desruisseaux@apache.org>
2013-05-07 16:13:42 +0000

    Initial commit of "storage/sis-netcdf" module, with only the attribute declarations for now.
    git-svn-id: https://svn.apache.org/repos/asf/sis/branches/JDK7@1479964 13f79535-47bb-0310-9956-ffa450edef68

---
+++ storage/sis-netcdf/src/main/java/org/apache/sis/storage/netcdf/package-info.java
@@ -0,0 +1,36 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Maps ISO metadata elements from/to the <a href="http://www.cfconventions.org">CF-compliant</a>
+ * attributes in a NetCDF file. The mapping is defined in the following web pages:
+ *
+ * <ul>
+ *   <li><a href="https://geo-ide.noaa.gov/wiki/index.php?title=NetCDF_Attribute_Convention_for_Dataset_Discovery">NetCDF
+ *       Attribute Convention for Dataset Discovery</a> wiki</li>
+ *   <li><a href="http://ngdc.noaa.gov/metadata/published/xsl/nciso2.0/UnidataDD2MI.xsl">UnidataDD2MI.xsl</a> file</li>
+ * </ul>
+ *
+ * The NetCDF attributes recognized by this package are listed in the
+ * {@link org.apache.sis.storage.netcdf.NetcdfMetadata} class.
+ *
+ * @author  Martin Desruisseaux (IRD, Geomatys)
+ * @since   0.3 (derived from geotk-3.20)
+ * @version 0.3
+ * @module
+ */
+package org.apache.sis.storage.netcdf;
