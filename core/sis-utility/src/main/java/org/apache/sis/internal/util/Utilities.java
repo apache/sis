21abae196c1be1d6dfb7821ae9cce280fc5b74d1
Martin Desruisseaux <desruisseaux@apache.org>
2013-05-17 20:57:05 +0000

    Added support for date/time units in ChannelDecoder.
    git-svn-id: https://svn.apache.org/repos/asf/sis/branches/JDK7@1483990 13f79535-47bb-0310-9956-ffa450edef68

--- core/sis-utility/src/main/java/org/apache/sis/internal/util/Utilities.java
+++ core/sis-utility/src/main/java/org/apache/sis/internal/util/Utilities.java
@@ -16,8 +16,10 @@
  */
 package org.apache.sis.internal.util;
 
+import java.util.Date;
 import java.util.Formatter;
 import java.util.FormattableFlags;
+import javax.xml.bind.DatatypeConverter;
 import org.apache.sis.util.Static;
 import org.apache.sis.util.CharSequences;
 
@@ -114,6 +116,103 @@ public final class Utilities extends Static {
         return (v1 instanceof Float || v1 instanceof Double) &&
                (v2 instanceof Float || v2 instanceof Double) &&
                epsilonEqual(((Number) v1).doubleValue(), ((Number) v2).doubleValue());
+    }
+
+    /**
+     * Parses a date from a string in ISO 8601 format. More specifically, this method expects the
+     * format defined by <cite>XML Schema Part 2: Datatypes for {@code xsd:dateTime}</cite>, with
+     * some additional flexibility (e.g. missing minutes or seconds fields are automatically added).
+     *
+     * @param  date The date to parse, or {@code null}.
+     * @param  defaultToUTC {@code true} if the default timezone shall be UTL, or {@code false} for locale default.
+     * @return The parsed date, or {@code null} if the given string was {@code null}.
+     * @throws IllegalArgumentException if the given date can not be parsed.
+     *
+     * @see DatatypeConverter#parseDateTime(String)
+     */
+    public static Date parseDateTime(String date, final boolean defaultToUTC) throws IllegalArgumentException {
+        if (date != null) {
+            date = CharSequences.trimWhitespaces(date);
+            if (!date.isEmpty()) {
+                /*
+                 * Check for missing time fields and time zone. For example if the given date is
+                 * "2005-09-22T00:00", then this block will complete it as "2005-09-22T00:00:00".
+                 * In addition, a 'Z' suffix will be appended if 'defaultToUTC' is true.
+                 */
+                int timeFieldStart  = date.lastIndexOf('T') + 1; // 0 if there is no time field.
+                int timeFieldEnd    = date.length();             // To be updated if there is a time field.
+                int missingFields   = 0;                         // Number of missing time fields.
+                boolean hasTimeZone = date.charAt(timeFieldEnd - 1) == 'Z';
+                if (timeFieldStart != 0) {
+                    if (hasTimeZone) {
+                        timeFieldEnd--;
+                    } else {
+                        final int s = Math.max(date.indexOf('+', timeFieldStart),
+                                               date.indexOf('-', timeFieldStart));
+                        hasTimeZone = (s >= 0);
+                        if (hasTimeZone) {
+                            timeFieldEnd = s;
+                        }
+                    }
+                    missingFields = 2;
+                    for (int i=timeFieldStart; i<timeFieldEnd; i++) {
+                        if (date.charAt(i) == ':') {
+                            if (--missingFields == 0) break;
+                        }
+                    }
+                }
+                /*
+                 * At this point, we have determined if there is some missing fields.
+                 * The timezone will be considered missing only if 'defaultToUTC' is true.
+                 */
+                CharSequence modified = date;
+                hasTimeZone |= !defaultToUTC;
+                if (missingFields != 0 || !hasTimeZone) {
+                    final StringBuilder buffer = new StringBuilder(date);
+                    while (--missingFields >= 0) {
+                        buffer.append(":00");
+                    }
+                    if (!hasTimeZone) {
+                        buffer.append('Z');
+                    }
+                    modified = buffer;
+                }
+                /*
+                 * Now ensure that all numbers have at least two digits.
+                 */
+                int indexOfLastDigit = 0;
+                for (int i=modified.length(); --i >= 0;) {
+                    char c = modified.charAt(i);
+                    final boolean isDigit = (c >= '0' && c <= '9'); // Do not use Character.isDigit(char).
+                    if (indexOfLastDigit == 0) {
+                        // We were not scaning a number. Check if we are now starting doing so.
+                        if (isDigit) {
+                            indexOfLastDigit = i;
+                        }
+                    } else {
+                        // We were scaning a number. Check if we found the begining.
+                        if (!isDigit) {
+                            if (indexOfLastDigit - i == 1) {
+                                // Reuse the buffer if it exists, or create a new one otherwise.
+                                final StringBuilder buffer;
+                                if (modified == date) {
+                                    modified = buffer = new StringBuilder(date);
+                                } else {
+                                    buffer = (StringBuilder) modified;
+                                }
+                                buffer.insert(i+1, '0');
+                            }
+                            indexOfLastDigit = 0;
+                        }
+                    }
+                }
+                /*
+                 * Now delegate to the utility method provided in the JAXB packages.
+                 */
+                return DatatypeConverter.parseDateTime(modified.toString()).getTime();
+            }
+        }
+        return null;
     }
 
     /**
