594b2a17ced6c92c44e37245b228d1333bda0e2c
Martin Desruisseaux <desruisseaux@apache.org>
2013-05-18 18:04:00 +0000

    Moved the temporary method (while waiting for JDK8) in its own "JDK8" internal class.
    git-svn-id: https://svn.apache.org/repos/asf/sis/branches/JDK7@1484156 13f79535-47bb-0310-9956-ffa450edef68

--- core/sis-utility/src/test/java/org/apache/sis/test/suite/UtilityTestSuite.java
+++ core/sis-utility/src/test/java/org/apache/sis/test/suite/UtilityTestSuite.java
@@ -51,6 +51,7 @@ import org.junit.BeforeClass;
     org.apache.sis.math.StatisticsTest.class,
     org.apache.sis.math.StatisticsFormatTest.class,
     org.apache.sis.internal.util.UtilitiesTest.class,
+    org.apache.sis.internal.jdk8.JDK8Test.class,
 
     // Collections.
     org.apache.sis.internal.util.ReferenceQueueConsumerTest.class,
