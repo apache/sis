cf29ffa7efb7d5d01cc752dbffa0407da08cb889
Martin Desruisseaux <desruisseaux@apache.org>
2013-07-12 09:25:21 +0000

    Upgraded version number from 0.3-SNAPSHOT to 0.4-SNAPSHOT.
    git-svn-id: https://svn.apache.org/repos/asf/sis/branches/JDK7@1502491 13f79535-47bb-0310-9956-ffa450edef68

--- core/sis-build-helper/src/main/java/org/apache/sis/internal/taglet/Module.java
+++ core/sis-build-helper/src/main/java/org/apache/sis/internal/taglet/Module.java
@@ -36,7 +36,7 @@ public final class Module implements Taglet {
     /**
      * SIS version to be referenced by this taglet.
      */
-    private static final String VERSION = "0.3-jdk7-SNAPSHOT";
+    private static final String VERSION = "0.4-jdk7-SNAPSHOT";
 
     /**
      * Register this taglet.
