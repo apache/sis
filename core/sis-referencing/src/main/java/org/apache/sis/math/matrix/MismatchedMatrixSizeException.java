f9b128f824f3995ec41d6e55fde2ec02b65484f5
Martin Desruisseaux <desruisseaux@apache.org>
2013-09-09 15:02:45 +0000

    Initial port of matrix interface. We plan to use JAMA under the hood as the initial implementation (except for very small matrix), but we keep the possibility to change implementation in the future.
    git-svn-id: https://svn.apache.org/repos/asf/sis/branches/JDK7@1521150 13f79535-47bb-0310-9956-ffa450edef68

---
+++ core/sis-referencing/src/main/java/org/apache/sis/math/matrix/MismatchedMatrixSizeException.java
@@ -0,0 +1,68 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.sis.math.matrix;
+
+
+/**
+ * Thrown when two matrix can not be added or multiplied because the sizes do not match.
+ *
+ * @author  Martin Desruisseaux (Geomatys)
+ * @since   0.4
+ * @version 0.4
+ * @module
+ */
+public class MismatchedMatrixSizeException extends IllegalArgumentException {
+    /**
+     * For cross-version compatibility.
+     */
+    private static final long serialVersionUID = -6055645640691056657L;
+
+    /**
+     * Constructs a new exception with no message.
+     */
+    public MismatchedMatrixSizeException() {
+        super();
+    }
+
+    /**
+     * Constructs a new exception with the specified detail message.
+     *
+     * @param message The detail message, or {@code null} if none.
+     */
+    public MismatchedMatrixSizeException(final String message) {
+        super(message);
+    }
+
+    /**
+     * Constructs a new exception with the specified detail message and cause.
+     *
+     * @param message The detail message, or {@code null} if none.
+     * @param cause The cause, or {@code null} if none.
+     */
+    public MismatchedMatrixSizeException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * Constructs a new exception with the specified cause.
+     *
+     * @param cause The cause, or {@code null} if none.
+     */
+    public MismatchedMatrixSizeException(final Throwable cause) {
+        super(cause);
+    }
+}
---
+++ core/sis-referencing/src/main/java/org/apache/sis/math/matrix/SingularMatrixException.java
@@ -0,0 +1,68 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.sis.math.matrix;
+
+
+/**
+ * Thrown when a matrix can not be inverted because it is singular.
+ *
+ * @author  Martin Desruisseaux (Geomatys)
+ * @since   0.4
+ * @version 0.4
+ * @module
+ */
+public class SingularMatrixException extends IllegalStateException {
+    /**
+     * For cross-version compatibility.
+     */
+    private static final long serialVersionUID = -4776332020257526053L;
+
+    /**
+     * Constructs a new exception with no message.
+     */
+    public SingularMatrixException() {
+        super();
+    }
+
+    /**
+     * Constructs a new exception with the specified detail message.
+     *
+     * @param message The detail message, or {@code null} if none.
+     */
+    public SingularMatrixException(final String message) {
+        super(message);
+    }
+
+    /**
+     * Constructs a new exception with the specified detail message and cause.
+     *
+     * @param message The detail message, or {@code null} if none.
+     * @param cause The cause, or {@code null} if none.
+     */
+    public SingularMatrixException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * Constructs a new exception with the specified cause.
+     *
+     * @param cause The cause, or {@code null} if none.
+     */
+    public SingularMatrixException(final Throwable cause) {
+        super(cause);
+    }
+}
