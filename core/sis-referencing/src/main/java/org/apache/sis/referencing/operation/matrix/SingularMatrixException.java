0ab6b447ec3f8c71b7db6dfd6de9082cc4f5fe20
Martin Desruisseaux <desruisseaux@apache.org>
2013-09-10 11:34:37 +0000

    Moved org.apache.sis.math.matrix to org.apache.sis.referencing.operation.matrix for the following reasons:
    1) Make clear that this is not a general-purpose matrix package, but something designed for the particular needs of referencing by coordinates.
    2) Make clear that those classes are defined in the sis-referencing module rather than sis-utility.
    
    Updated package-info.java javadoc for explaining some of the above.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/sis/branches/JDK7@1521437 13f79535-47bb-0310-9956-ffa450edef68

--- core/sis-referencing/src/main/java/org/apache/sis/referencing/operation/matrix/SingularMatrixException.java
+++ core/sis-referencing/src/main/java/org/apache/sis/referencing/operation/matrix/SingularMatrixException.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.sis.math.matrix;
+package org.apache.sis.referencing.operation.matrix;
 
 
 /**
