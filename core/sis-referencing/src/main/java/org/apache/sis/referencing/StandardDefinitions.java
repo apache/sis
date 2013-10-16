29017a3a7d33a1301afd97331708a4a3fa6d6cd0
Martin Desruisseaux <desruisseaux@apache.org>
2013-10-16 19:27:41 +0000

    Trivial reformating for making the lines a little bit shorter.
    git-svn-id: https://svn.apache.org/repos/asf/sis/branches/JDK7@1532872 13f79535-47bb-0310-9956-ffa450edef68

--- core/sis-referencing/src/main/java/org/apache/sis/referencing/StandardDefinitions.java
+++ core/sis-referencing/src/main/java/org/apache/sis/referencing/StandardDefinitions.java
@@ -80,12 +80,12 @@ final class StandardDefinitions {
         boolean ivfDefinitive  = true;
         Unit<Length> unit      = SI.METRE;
         switch (code) {
-            case 7030: name  = "WGS 84";                   alias = "WGS84";              semiMajorAxis = 6378137.0; other = 298.257223563; break;
-            case 7043: name  = "WGS 72";                   alias = "NWL 10D";            semiMajorAxis = 6378135.0; other = 298.26;        break;
-            case 7019: name  = "GRS 1980";                 alias = "International 1979"; semiMajorAxis = 6378137.0; other = 298.257222101; break;
-            case 7022: alias = "Hayford 1909";             name  = "International 1924"; semiMajorAxis = 6378388.0; other = 297.0;         break;
-            case 7008: name  = "Clarke 1866";              ivfDefinitive = false;        semiMajorAxis = 6378206.4; other = 6356583.8;     break;
-            case 7048: name  = "GRS 1980 Authalic Sphere"; ivfDefinitive = false;        semiMajorAxis =            other = 6371007;       break;
+            case 7030: name  = "WGS 84";                   alias = "WGS84";        semiMajorAxis = 6378137.0; other = 298.257223563; break;
+            case 7043: name  = "WGS 72";                   alias = "NWL 10D";      semiMajorAxis = 6378135.0; other = 298.26;        break;
+            case 7019: alias = "International 1979";       name  = "GRS 1980";     semiMajorAxis = 6378137.0; other = 298.257222101; break;
+            case 7022: name  = "International 1924";       alias = "Hayford 1909"; semiMajorAxis = 6378388.0; other = 297.0;         break;
+            case 7008: name  = "Clarke 1866";              ivfDefinitive = false;  semiMajorAxis = 6378206.4; other = 6356583.8;     break;
+            case 7048: name  = "GRS 1980 Authalic Sphere"; ivfDefinitive = false;  semiMajorAxis =            other = 6371007;       break;
             default:   throw new AssertionError(code);
         }
         final Map<String,Object> map = new HashMap<>(8);
