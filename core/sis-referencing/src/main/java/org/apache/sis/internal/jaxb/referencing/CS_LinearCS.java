0c00018719c2f9519c134dd5b91dbccf65c1cdf5
Martin Desruisseaux <desruisseaux@apache.org>
2014-01-05 06:24:35 +0000

    Ported EngineeringCRS.
    git-svn-id: https://svn.apache.org/repos/asf/sis/branches/JDK7@1555462 13f79535-47bb-0310-9956-ffa450edef68

---
+++ core/sis-referencing/src/main/java/org/apache/sis/internal/jaxb/referencing/CS_LinearCS.java
@@ -0,0 +1,92 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.sis.internal.jaxb.referencing;
+
+import javax.xml.bind.annotation.XmlElement;
+import org.opengis.referencing.cs.LinearCS;
+import org.apache.sis.referencing.cs.DefaultLinearCS;
+import org.apache.sis.internal.jaxb.gco.PropertyType;
+
+
+/**
+ * JAXB adapter mapping implementing class to the GeoAPI interface. See
+ * package documentation for more information about JAXB and interface.
+ *
+ * @author  Martin Desruisseaux (Geomatys)
+ * @since   0.4
+ * @version 0.4
+ * @module
+ */
+public final class CS_LinearCS extends PropertyType<CS_LinearCS, LinearCS> {
+    /**
+     * Empty constructor for JAXB only.
+     */
+    public CS_LinearCS() {
+    }
+
+    /**
+     * Returns the GeoAPI interface which is bound by this adapter.
+     * This method is indirectly invoked by the private constructor
+     * below, so it shall not depend on the state of this object.
+     *
+     * @return {@code LinearCS.class}
+     */
+    @Override
+    protected Class<LinearCS> getBoundType() {
+        return LinearCS.class;
+    }
+
+    /**
+     * Constructor for the {@link #wrap} method only.
+     */
+    private CS_LinearCS(final LinearCS cs) {
+        super(cs);
+    }
+
+    /**
+     * Invoked by {@link PropertyType} at marshalling time for wrapping the given value
+     * in a {@code <gml:LinearCS>} XML element.
+     *
+     * @param  cs The element to marshall.
+     * @return A {@code PropertyType} wrapping the given the element.
+     */
+    @Override
+    protected CS_LinearCS wrap(final LinearCS cs) {
+        return new CS_LinearCS(cs);
+    }
+
+    /**
+     * Invoked by JAXB at marshalling time for getting the actual element to write
+     * inside the {@code <gml:LinearCS>} XML element.
+     * This is the value or a copy of the value given in argument to the {@code wrap} method.
+     *
+     * @return The element to be marshalled.
+     */
+    @XmlElement(name = "LinearCS")
+    public DefaultLinearCS getElement() {
+        return DefaultLinearCS.castOrCopy(metadata);
+    }
+
+    /**
+     * Invoked by JAXB at unmarshalling time for storing the result temporarily.
+     *
+     * @param cs The unmarshalled element.
+     */
+    public void setElement(final DefaultLinearCS cs) {
+        metadata = cs;
+    }
+}
