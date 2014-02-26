eb4d4139303a1542b246fe3d655c3533f4290f0f
Martin Desruisseaux <desruisseaux@apache.org>
2014-02-26 16:28:22 +0000

    Remove the site/apt/index.apt files that do not provide any useful information, or APT files that repeat the information existing in the package-info.java file.
    Rational: those APT files were created in older time where the Maven generated site
    was our main web site. This is no longer the case, and the Maven generated site is
    now used only by a few developers. Since we should not put links from the public
    site to the Jenkins build server (which host the Maven site), we should not have
    any information for users in API files.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/sis/branches/JDK7@1572147 13f79535-47bb-0310-9956-ffa450edef68

--- storage/sis-netcdf/src/main/java/org/apache/sis/storage/netcdf/package-info.java
+++ storage/sis-netcdf/src/main/java/org/apache/sis/storage/netcdf/package-info.java
@@ -16,7 +16,7 @@
  */
 
 /**
- * Maps ISO metadata elements from/to the <a href="http://www.cfconventions.org">CF-compliant</a>
+ * Maps ISO metadata elements from/to the <a href="http://www.cfconventions.org">Climate and Forecast (CF)</a>
  * attributes in a NetCDF file. The mapping is defined in the following web pages:
  *
  * <ul>
@@ -27,6 +27,25 @@
  *
  * The NetCDF attributes recognized by this package are listed in the
  * {@link org.apache.sis.storage.netcdf.AttributeNames} class.
+ *
+ * {@section Note on the definition of terms}
+ * The UCAR library sometime uses the same words than the ISO/OGC standards for different things.
+ * In particular the words "<cite>domain</cite>" and "<cite>range</cite>" can be applied to arbitrary functions,
+ * and the UCAR library chooses to apply it to the function that converts grid indices to geodetic coordinates.
+ * The ISO 19123 standard on the other hand considers coverage as a function, and applies those <cite>domain</cite>
+ * and <cite>range</cite> words to that function. More specifically:
+ *
+ * <ul>
+ *   <li>UCAR "<cite>coordinate system</cite>" is actually a mix of <cite>coordinate system</cite>,
+ *       <cite>coordinate reference system</cite> and <cite>grid geometry</cite> in OGC sense.</li>
+ *   <li>UCAR coordinate system "<cite>domain</cite>" is not equivalent to ISO 19123 coverage domain,
+ *       but is rather related to <cite>grid envelope</cite>.</li>
+ *   <li>ISO 19123 coverage <cite>domain</cite> is related to UCAR coordinate system "<cite>range</cite>".</li>
+ *   <li>ISO 19123 coverage <cite>range</cite> is not equivalent to UCAR "<cite>range</cite>",
+ *       but is rather related to the NetCDF variable's minimum and maximum values.</li>
+ * </ul>
+ *
+ * Care must be taken for avoiding confusion when using SIS and UCAR libraries together.
  *
  * @author  Martin Desruisseaux (IRD, Geomatys)
  * @since   0.3 (derived from geotk-3.20)
