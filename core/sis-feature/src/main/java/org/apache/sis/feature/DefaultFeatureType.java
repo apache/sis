1f919cda62d61dbab964251c78c5a9dc19bc08ad
Martin Desruisseaux <desruisseaux@apache.org>
2014-03-06 16:47:22 +0000

    First draft of a DefaultFeatureType implementation containing the attributes.
    git-svn-id: https://svn.apache.org/repos/asf/sis/branches/JDK7@1574958 13f79535-47bb-0310-9956-ffa450edef68

--- core/sis-feature/src/main/java/org/apache/sis/feature/DefaultFeatureType.java
+++ core/sis-feature/src/main/java/org/apache/sis/feature/DefaultFeatureType.java
@@ -17,6 +17,14 @@
 package org.apache.sis.feature;
 
 import java.util.Map;
+import java.util.Set;
+import java.util.List;
+import java.util.Collections;
+import org.opengis.util.GenericName;
+import org.apache.sis.util.ArgumentChecks;
+import org.apache.sis.util.resources.Errors;
+import org.apache.sis.internal.util.CollectionsExt;
+import org.apache.sis.internal.util.UnmodifiableArrayList;
 
 
 /**
@@ -38,6 +46,24 @@ import java.util.Map;
  * @module
  */
 public class DefaultFeatureType extends AbstractIdentifiedType {
+    /**
+     * If {@code true}, the feature type acts as an abstract super-type.
+     *
+     * @see #isAbstract()
+     */
+    private final boolean isAbstract;
+
+    /**
+     * The parents of this feature type, or an empty set if none.
+     */
+    private final Set<DefaultFeatureType> superTypes;
+
+    /**
+     * Any feature operation, any feature attribute type and any feature association role
+     * that carries characteristics of a feature type.
+     */
+    private final List<DefaultAttributeType<?>> characteristics;
+
     /**
      * Constructs a feature type from the given properties. The properties map is given unchanged to
      * the {@linkplain AbstractIdentifiedType#AbstractIdentifiedType(Map) super-class constructor}.
@@ -72,8 +98,70 @@ public class DefaultFeatureType extends AbstractIdentifiedType {
      * </table>
      *
      * @param properties The name and other properties to be given to this feature type.
+     * @param isAbstract If {@code true}, the feature type acts as an abstract super-type.
+     * @param superTypes The parents of this feature type, or {@code null} or empty if none.
+     * @param characteristics Any feature operation, any feature attribute type and any feature
+     *        association role that carries characteristics of a feature type.
      */
-    public DefaultFeatureType(final Map<String,?> properties) {
+    public DefaultFeatureType(final Map<String,?> properties, final boolean isAbstract,
+            final DefaultFeatureType[] superTypes, final DefaultAttributeType... characteristics)
+    {
         super(properties);
+        ArgumentChecks.ensureNonNull("characteristics", characteristics);
+        this.isAbstract = isAbstract;
+        this.superTypes = (superTypes == null) ? Collections.<DefaultFeatureType>emptySet() :
+                          CollectionsExt.<DefaultFeatureType>immutableSet(true, superTypes);
+        final DefaultAttributeType<?>[] copy = new DefaultAttributeType<?>[characteristics.length];
+        for (int i=0; i<characteristics.length; i++) {
+            copy[i] = characteristics[i];
+            ArgumentChecks.ensureNonNullElement("characteristics", i, copy);
+            /*
+             * Ensure there is no conflict in property names.
+             */
+            final GenericName name = copy[i].getName();
+            if (name == null) {
+                throw new IllegalArgumentException(Errors.format(
+                        Errors.Keys.MissingValueForProperty_1, "characteristics[" + i + "].name"));
+            }
+            for (int j=i; --j >= 0;) {
+                if (name.equals(copy[j].getName())) {
+                    throw new IllegalArgumentException(Errors.format(
+                            Errors.Keys.DuplicatedIdentifier_1, name));
+                }
+            }
+        }
+        this.characteristics = UnmodifiableArrayList.wrap(copy);
+    }
+
+    /**
+     * Returns {@code true} if the feature type acts as an abstract super-type.
+     *
+     * @return {@code true} if the feature type acts as an abstract super-type.
+     */
+    public boolean isAbstract() {
+        return isAbstract;
+    }
+
+    /**
+     * Returns the parents of this feature type.
+     *
+     * @return The parents of this feature type, or an empty set if none.
+     */
+    public Set<DefaultFeatureType> superTypes() {
+        return superTypes;
+    }
+
+    /**
+     * Returns any feature operation, any feature attribute type and any feature association role
+     * that carries characteristics of a feature type.
+     *
+     * <div class="warning"><b>Warning:</b>
+     * The type of list elements will be changed to {@code PropertyType} if and when such interface
+     * will be defined in GeoAPI.</div>
+     *
+     * @return Feature operation, attribute type and association role that carries characteristics of a feature type.
+     */
+    public List<DefaultAttributeType<?>> getCharacteristics() {
+        return characteristics;
     }
 }
