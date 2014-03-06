2d16b5b6445d31d177fd58ad0ae340393fb389f2
Martin Desruisseaux <desruisseaux@apache.org>
2014-03-06 22:51:28 +0000

    Fixed javadoc sentence.
    git-svn-id: https://svn.apache.org/repos/asf/sis/branches/JDK7@1575085 13f79535-47bb-0310-9956-ffa450edef68

--- core/sis-metadata/src/main/java/org/apache/sis/metadata/iso/constraint/DefaultConstraints.java
+++ core/sis-metadata/src/main/java/org/apache/sis/metadata/iso/constraint/DefaultConstraints.java
@@ -90,7 +90,7 @@ public class DefaultConstraints extends ISOMetadata implements Constraints {
 
     /**
      * Returns a SIS metadata implementation with the values of the given arbitrary implementation.
-     * This method performs the first applicable actions in the following choices:
+     * This method performs the first applicable action in the following choices:
      *
      * <ul>
      *   <li>If the given object is {@code null}, then this method returns {@code null}.</li>
--- core/sis-metadata/src/main/java/org/apache/sis/metadata/iso/quality/DefaultScope.java
+++ core/sis-metadata/src/main/java/org/apache/sis/metadata/iso/quality/DefaultScope.java
@@ -98,7 +98,7 @@ public class DefaultScope extends ISOMetadata implements Scope {
 
     /**
      * Returns a SIS metadata implementation with the values of the given arbitrary implementation.
-     * This method performs the first applicable actions in the following choices:
+     * This method performs the first applicable action in the following choices:
      *
      * <ul>
      *   <li>If the given object is {@code null}, then this method returns {@code null}.</li>
