a77e8128a82070dea917942268060f37f5952989
Martin Desruisseaux <desruisseaux@apache.org>
2014-04-29 21:05:49 +0000

    Starting application of DefaultFeature changes proposed by email on February 24th: Replaced getRecord() et setRecord(Map) methods by getAttributeValue(String) and setAttributeValue(String, Object).
    git-svn-id: https://svn.apache.org/repos/asf/sis/branches/JDK8@1591104 13f79535-47bb-0310-9956-ffa450edef68

--- core/sis-feature/src/main/java/org/apache/sis/feature/DefaultFeature.java
+++ core/sis-feature/src/main/java/org/apache/sis/feature/DefaultFeature.java
@@ -18,28 +18,67 @@ package org.apache.sis.feature;
 
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Collections;
 import com.esri.core.geometry.Geometry;
+import org.apache.sis.util.ArgumentChecks;
 
 
 /**
- * Simple Feature class.
+ * An instance of {@linkplain DefaultFeatureType feature type} containing values for a real-world phenomena.
  *
  * @author  Travis L. Pinney
+ * @author  Johann Sorel (Geomatys)
+ * @author  Martin Desruisseaux (Geomatys)
  * @since   0.5
  * @version 0.5
  * @module
  */
 public class DefaultFeature {
+    /**
+     * The properties (attributes, operations, feature associations) of this feature.
+     */
+    private final Map<String, DefaultAttribute<?>> properties;
 
-    private Map<String, String> record;
     private Geometry geom;
 
-    public Map<String, String> getRecord() {
-        return record;
+    /**
+     * Creates a new features.
+     */
+    public DefaultFeature() {
+        properties = new HashMap<>();
     }
 
-    public void setRecord(HashMap<String, String> record) {
-        this.record = record;
+    /**
+     * Returns the value of the attribute of the given name.
+     *
+     * @param  name The attribute name.
+     * @return The value for the given attribute, or {@code null} if none.
+     */
+    public Object getAttributeValue(final String name) {
+        final DefaultAttribute<?> attribute = properties.get(name);
+        return (attribute != null) ? attribute.getValue() : null;
+    }
+
+    /**
+     * Sets the value of the attribute of the given name.
+     *
+     * @param name The attribute name.
+     * @param value The new value for the given attribute.
+     */
+    @SuppressWarnings("unchecked")
+    public void setAttributeValue(final String name, final Object value) {
+        DefaultAttribute<?> attribute = properties.get(name);
+        if (attribute == null) {
+            if (value == null) {
+                return;
+            }
+            // TEMPORARY HACK: FeatureType should be defined at construction time.
+            final DefaultAttributeType<Object> type = new DefaultAttributeType<>(
+                    Collections.singletonMap("name", name), Object.class, null, null);
+            attribute = new DefaultAttribute<>(type);
+        }
+        ArgumentChecks.ensureCanCast(name, attribute.getType().getValueClass(), value);
+        ((DefaultAttribute) attribute).setValue(value);
     }
 
     public Geometry getGeom() {
@@ -54,8 +93,8 @@ public class DefaultFeature {
     public String toString() {
         final StringBuilder sb = new StringBuilder();
         final String lineSeparator = System.lineSeparator();
-        for (String s : this.record.keySet()) {
-            sb.append(s).append(": ").append(record.get(s)).append(lineSeparator);
+        for (final DefaultAttribute<?> attribute : properties.values()) {
+            sb.append(attribute.getType().getName()).append(": ").append(attribute.getValue()).append(lineSeparator);
         }
         return sb.toString();
     }
