9d0ed0e2a4c65cabe760f920bce59f53d0ee01f6
Martin Desruisseaux <desruisseaux@apache.org>
2014-06-04 17:45:46 +0000

    Ported the code that apply a transformation on a Java2D Shape.
    git-svn-id: https://svn.apache.org/repos/asf/sis/branches/JDK8@1600392 13f79535-47bb-0310-9956-ffa450edef68

--- core/sis-referencing/src/main/java/org/apache/sis/referencing/operation/transform/AbstractMathTransform2D.java
+++ core/sis-referencing/src/main/java/org/apache/sis/referencing/operation/transform/AbstractMathTransform2D.java
@@ -19,11 +19,19 @@ package org.apache.sis.referencing.operation.transform;
 import java.util.List;
 import java.awt.Shape;
 import java.awt.geom.Point2D;
+import java.awt.geom.Line2D;
+import java.awt.geom.QuadCurve2D;
+import java.awt.geom.Path2D;
+import java.awt.geom.PathIterator;
 import java.awt.geom.AffineTransform;
+import java.awt.geom.IllegalPathStateException;
+import org.opengis.referencing.operation.Matrix;
 import org.opengis.referencing.operation.MathTransform;
 import org.opengis.referencing.operation.MathTransform2D;
 import org.opengis.referencing.operation.TransformException;
 import org.opengis.referencing.operation.NoninvertibleTransformException;
+import org.apache.sis.internal.referencing.ShapeUtilities;
+import org.apache.sis.util.resources.Errors;
 
 
 /**
@@ -102,10 +110,10 @@ public abstract class AbstractMathTransform2D extends AbstractMathTransform impl
 
     /**
      * Transforms a geometric shape. This method always copy transformed coordinates in a new object.
-     * The new object is often a {@link Path2D}, but may also be a {@link Line2D} or a
-     * {@link QuadCurve2D} if such simplification is possible.
+     * The new object is often a {@link Path2D}, but may also be a {@link Line2D} or a {@link QuadCurve2D}
+     * if such simplification is possible.
      *
-     * @param  transform     The transform to use.
+     * @param  mt            The math transform to use.
      * @param  shape         The geometric shape to transform.
      * @param  preTransform  An optional affine transform to apply <em>before</em> the
      *                       transformation using {@code this}, or {@code null} if none.
@@ -114,18 +122,155 @@ public abstract class AbstractMathTransform2D extends AbstractMathTransform impl
      * @param  horizontal    {@code true} for forcing parabolic equation.
      *
      * @return The transformed geometric shape.
-     * @throws MismatchedDimensionException if this transform doesn't is not two-dimensional.
      * @throws TransformException If a transformation failed.
      */
-    static Shape createTransformedShape(final MathTransform2D transform,
+    static Shape createTransformedShape(final MathTransform2D mt,
                                         final Shape           shape,
                                         final AffineTransform preTransform,
                                         final AffineTransform postTransform,
                                         final boolean         horizontal)
             throws TransformException
     {
-        // TODO
-        throw new UnsupportedOperationException();
+        final PathIterator     it = shape.getPathIterator(preTransform);
+        final Path2D.Double  path = new Path2D.Double(it.getWindingRule());
+        final double[]     buffer = new double[6];
+
+        double ax=0, ay=0;  // Coordinate of the last point before transform.
+        double px=0, py=0;  // Coordinate of the last point after  transform.
+        for (; !it.isDone(); it.next()) {
+            switch (it.currentSegment(buffer)) {
+                default: {
+                    throw new IllegalPathStateException();
+                }
+                case PathIterator.SEG_CLOSE: {
+                    /*
+                     * Close the geometric shape and continues the loop. We use the 'continue' instruction
+                     * here instead of 'break' because we do not want to execute the code after the switch
+                     * (addition of transformed points into the path - there is no such point in a SEG_CLOSE).
+                     */
+                    path.closePath();
+                    continue;
+                }
+                case PathIterator.SEG_MOVETO: {
+                    /*
+                     * Transform the single point and adds it to the path. We use the 'continue' instruction
+                     * here instead of 'break' because we do not want to execute the code after the switch
+                     * (addition of a line or a curve - there is no such curve to add here; we are just moving
+                     * the cursor).
+                     */
+                    ax = buffer[0];
+                    ay = buffer[1];
+                    mt.transform(buffer, 0, buffer, 0, 1);
+                    px = buffer[0];
+                    py = buffer[1];
+                    path.moveTo(px, py);
+                    continue;
+                }
+                case PathIterator.SEG_LINETO: {
+                    /*
+                     * Insert a new control point at 'buffer[0,1]'. This control point will
+                     * be initialised with coordinates in the middle of the straight line:
+                     *
+                     *  x = 0.5 * (x1+x2)
+                     *  y = 0.5 * (y1+y2)
+                     *
+                     * This point will be transformed after the 'switch', which is why we use
+                     * the 'break' statement here instead of 'continue' as in previous case.
+                     */
+                    buffer[0] = 0.5 * (ax + (ax = buffer[0]));
+                    buffer[1] = 0.5 * (ay + (ay = buffer[1]));
+                    buffer[2] = ax;
+                    buffer[3] = ay;
+                    break;
+                }
+                case PathIterator.SEG_QUADTO: {
+                    /*
+                     * Replace the control point in 'buffer[0,1]' by a new control point lying on the quadratic curve.
+                     * Coordinates for a point in the middle of the curve can be computed with:
+                     *
+                     *  x = 0.5 * (ctrlx + 0.5 * (x1+x2))
+                     *  y = 0.5 * (ctrly + 0.5 * (y1+y2))
+                     *
+                     * There is no need to keep the old control point because it was not lying on the curve.
+                     */
+                    buffer[0] = 0.5 * (buffer[0] + 0.5*(ax + (ax = buffer[2])));
+                    buffer[1] = 0.5 * (buffer[1] + 0.5*(ay + (ay = buffer[3])));
+                    break;
+                }
+                case PathIterator.SEG_CUBICTO: {
+                    /*
+                     * Replace the control point in 'buffer[0,1]' by a new control point lying on the cubic curve.
+                     * Coordinates for a point in the middle of the curve can be computed with:
+                     *
+                     *  x = 0.25 * (1.5 * (ctrlx1 + ctrlx2) + 0.5 * (x1 + x2));
+                     *  y = 0.25 * (1.5 * (ctrly1 + ctrly2) + 0.5 * (y1 + y2));
+                     *
+                     * There is no need to keep the old control point because it was not lying on the curve.
+                     *
+                     * NOTE: The computed point is on the curve, but may not be representative of the shape.
+                     *       This algorithm replaces two control points by a single one, because we did not
+                     *       venture into a more sophisticated algorithm producing a CubicCurve2D. For now,
+                     *       we presume that the current algorithm is okay if the curve is smooth enough.
+                     */
+                    buffer[0] = 0.25 * (1.5 * (buffer[0] + buffer[2]) + 0.5 * (ax + (ax = buffer[4])));
+                    buffer[1] = 0.25 * (1.5 * (buffer[1] + buffer[3]) + 0.5 * (ay + (ay = buffer[5])));
+                    buffer[2] = ax;
+                    buffer[3] = ay;
+                    break;
+                }
+            }
+            /*
+             * Apply the transform on the point in the buffer, and append the transformed points
+             * to the general path. Try to add them as a quadratic line, or as a straight line if
+             * the computed control point is colinear with the starting and ending points.
+             */
+            mt.transform(buffer, 0, buffer, 0, 2);
+            final Point2D ctrlPoint = ShapeUtilities.parabolicControlPoint(px, py,
+                    buffer[0], buffer[1],
+                    buffer[2], buffer[3],
+                    horizontal);
+            px = buffer[2];
+            py = buffer[3];
+            if (ctrlPoint != null) {
+                path.quadTo(ctrlPoint.getX(), ctrlPoint.getY(), px, py);
+            } else {
+                path.lineTo(px, py);
+            }
+        }
+        /*
+         * Shape transformation is done. Apply an affine transform if it was requested,
+         * then simplify the geometric object (not the coordinate values) if possible.
+         */
+        if (postTransform != null) {
+            path.transform(postTransform);
+        }
+        return ShapeUtilities.toPrimitive(path);
+    }
+
+    /**
+     * Gets the derivative of this transform at a point.
+     * The default implementation performs the following steps:
+     *
+     * <ul>
+     *   <li>Copy the coordinate in a temporary array and pass that array to the
+     *       {@link #transform(double[], int, double[], int, boolean)} method,
+     *       with the {@code derivate} boolean argument set to {@code true}.</li>
+     *   <li>If the later method returned a non-null matrix, returns that matrix.
+     *       Otherwise throws {@link TransformException}.</li>
+     * </ul>
+     *
+     * @param  point The coordinate point where to evaluate the derivative.
+     * @return The derivative at the specified point as a 2×2 matrix.
+     * @throws TransformException if the derivative can not be evaluated at the specified point.
+     */
+    @Override
+    public Matrix derivative(final Point2D point) throws TransformException {
+        final double[] coordinate = new double[] {point.getX(), point.getY()};
+        final Matrix derivative = transform(coordinate, 0, null, 0, true);
+        if (derivative == null) {
+            throw new TransformException(Errors.format(Errors.Keys.CanNotComputeDerivative));
+        }
+        return derivative;
     }
 
     /**
