559cb27ee96bf54210540d53184f36f128e41500
Martin Desruisseaux <desruisseaux@apache.org>
2014-09-22 11:22:32 +0000

    Reduce usage of deprecated methods or patterns.
    git-svn-id: https://svn.apache.org/repos/asf/sis/branches/JDK8@1626733 13f79535-47bb-0310-9956-ffa450edef68

--- core/sis-metadata/src/test/java/org/apache/sis/metadata/iso/CustomMetadataTest.java
+++ core/sis-metadata/src/test/java/org/apache/sis/metadata/iso/CustomMetadataTest.java
@@ -42,6 +42,7 @@ import org.apache.sis.test.XMLTestCase;
 import org.apache.sis.xml.XML;
 import org.junit.Test;
 
+import static java.util.Collections.singleton;
 import static org.junit.Assert.*;
 
 
@@ -136,7 +137,7 @@ public final strictfp class CustomMetadataTest extends XMLTestCase {
             @Override public Collection<Citation>                  getAdditionalDocumentations()   {return null;}
         };
         final DefaultMetadata data = new DefaultMetadata();
-        assertTrue(data.getIdentificationInfo().add(identification));
+        data.setIdentificationInfo(singleton(identification));
         final String xml = XML.marshal(data);
         /*
          * A few simple checks.
