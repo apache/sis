774621c76b56b8eba88c2350f50afb8032f27870
Martin Desruisseaux <desruisseaux@apache.org>
2015-01-26 18:45:00 +0000

    Moved org.geotoolkit.referencing.operation.provider to an internal package.
    git-svn-id: https://svn.apache.org/repos/asf/sis/branches/JDK8@1654866 13f79535-47bb-0310-9956-ffa450edef68

--- core/sis-referencing/src/main/java/org/apache/sis/internal/referencing/provider/Affine.java
+++ core/sis-referencing/src/main/java/org/apache/sis/internal/referencing/provider/Affine.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.sis.referencing.operation.provider;
+package org.apache.sis.internal.referencing.provider;
 
 import java.util.Map;
 import java.util.Collections;
