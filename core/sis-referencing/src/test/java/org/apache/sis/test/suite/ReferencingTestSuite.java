8aa24e59ef795940a54d03979b3855796660ccd5
Martin Desruisseaux <desruisseaux@apache.org>
2015-02-16 15:31:05 +0000

    Initial draft of DefaultMathTransformFactory tests. Will be completed later.
    git-svn-id: https://svn.apache.org/repos/asf/sis/branches/JDK8@1660141 13f79535-47bb-0310-9956-ffa450edef68

--- core/sis-referencing/src/test/java/org/apache/sis/test/suite/ReferencingTestSuite.java
+++ core/sis-referencing/src/test/java/org/apache/sis/test/suite/ReferencingTestSuite.java
@@ -80,6 +80,7 @@ import org.junit.BeforeClass;
     org.apache.sis.referencing.operation.DefaultFormulaTest.class,
     org.apache.sis.referencing.operation.DefaultOperationMethodTest.class,
     org.apache.sis.referencing.operation.transform.OperationMethodSetTest.class,
+    org.apache.sis.referencing.operation.transform.DefaultMathTransformFactoryTest.class,
     org.apache.sis.internal.referencing.OperationMethodsTest.class,
 
     org.apache.sis.referencing.datum.BursaWolfParametersTest.class,
