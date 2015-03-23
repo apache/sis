51c508f7f2ac8deef34cfbbe932554fdb15fdff6
Martin Desruisseaux <desruisseaux@apache.org>
2015-03-23 10:34:43 +0000

    Referencing: enabled the JUnit tests in UnitaryProjection which were ignored.
    git-svn-id: https://svn.apache.org/repos/asf/sis/branches/JDK8@1668585 13f79535-47bb-0310-9956-ffa450edef68

---
+++ core/sis-referencing/src/test/java/org/apache/sis/referencing/operation/projection/NoOp.java
@@ -0,0 +1,89 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.sis.referencing.operation.projection;
+
+import java.util.Collections;
+import org.opengis.parameter.ParameterValueGroup;
+import org.opengis.referencing.operation.Matrix;
+import org.apache.sis.referencing.operation.DefaultOperationMethod;
+import org.apache.sis.parameter.ParameterBuilder;
+import org.apache.sis.parameter.Parameters;
+import org.apache.sis.internal.util.Constants;
+import org.apache.sis.util.Workaround;
+
+
+/**
+ * A simple implementation of {@link UnitaryProjection} as a "no-operation".
+ * This is used for testing methods other than {@code transform(…)} and {@code inverseTransform(…)}.
+ *
+ * @author  Martin Desruisseaux (Geomatys)
+ * @since   0.6
+ * @version 0.6
+ * @module
+ */
+@SuppressWarnings("serial")
+final strictfp class NoOp extends UnitaryProjection {
+    /**
+     * Creates a new "no-operation".
+     *
+     * @param ellipsoidal {@code true} for an ellipsoidal case, or {@code false} for a spherical case.
+     */
+    NoOp(final boolean ellipsoidal) {
+        this(parameters(ellipsoidal));
+    }
+
+    /**
+     * Work around for RFE #4093999 in Sun's bug database
+     * ("Relax constraint on placement of this()/super() call in constructors").
+     */
+    @Workaround(library="JDK", version="1.7")
+    private NoOp(final Parameters parameters) {
+        super(new DefaultOperationMethod(
+                Collections.singletonMap(DefaultOperationMethod.NAME_KEY, parameters.getDescriptor().getName()),
+                2, 2, parameters.getDescriptor()), parameters);
+    }
+
+    /**
+     * Work around for RFE #4093999 in Sun's bug database
+     * ("Relax constraint on placement of this()/super() call in constructors").
+     */
+    @Workaround(library="JDK", version="1.7")
+    private static Parameters parameters(final boolean ellipsoidal) {
+        final ParameterValueGroup group = new ParameterBuilder()
+                .addName("No-operation").createGroupForMapProjection().createValue();
+        group.parameter(Constants.SEMI_MAJOR).setValue(ellipsoidal ? 6378137 : 6371007);
+        group.parameter(Constants.SEMI_MINOR).setValue(ellipsoidal ? 6356752 : 6371007);
+        return (Parameters) group;
+    }
+
+    /**
+     * Do nothing.
+     *
+     * @return {@code null}.
+     */
+    @Override
+    public Matrix transform(double[] srcPts, int srcOff, double[] dstPts, int dstOff, boolean derivate) {
+        return null;
+    }
+
+    /**
+     * Do nothing.
+     */
+    @Override
+    protected void inverseTransform(double[] srcPts, int srcOff, double[] dstPts, int dstOff) {
+    }
+}
