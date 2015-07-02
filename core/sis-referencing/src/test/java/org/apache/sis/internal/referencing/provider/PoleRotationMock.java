e60b92c01f839c839a5bd4866426d883caa7ee6b
Martin Desruisseaux <desruisseaux@apache.org>
2015-07-02 08:03:15 +0000

    WKT 2: add a mock for the "Pole rotation" operation method. This allow us to execute the GeoAPI test for Derived Geodetic CRS.
    git-svn-id: https://svn.apache.org/repos/asf/sis/branches/JDK8@1688778 13f79535-47bb-0310-9956-ffa450edef68

---
+++ core/sis-referencing/src/test/java/org/apache/sis/internal/referencing/provider/PoleRotationMock.java
@@ -0,0 +1,88 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.sis.internal.referencing.provider;
+
+import javax.measure.unit.NonSI;
+import org.opengis.parameter.ParameterDescriptor;
+import org.opengis.parameter.ParameterDescriptorGroup;
+import org.opengis.parameter.ParameterValueGroup;
+import org.opengis.referencing.operation.Matrix;
+import org.opengis.referencing.operation.MathTransform;
+import org.opengis.referencing.operation.MathTransformFactory;
+import org.apache.sis.referencing.operation.transform.AbstractMathTransform;
+import org.apache.sis.parameter.ParameterBuilder;
+
+
+/**
+ * The provider for <cite>"Pole rotation"</cite> conversion.
+ *
+ * This conversion is not yet implemented in Apache SIS, but we need to at least accept the parameters
+ * for a Well Known Text (WKT) parsing test in the {@link org.apache.sis.io.wkt.WKTParserTest} class.
+ *
+ * <p>This class may be promoted to a real operation if we implement the formulas in a future Apache SIS version.</p>
+ *
+ * @author  Martin Desruisseaux (Geomatys)
+ * @since   0.6
+ * @version 0.6
+ * @module
+ */
+@SuppressWarnings("serial")
+public final strictfp class PoleRotationMock extends AbstractProvider {
+    /**
+     * The group of all parameters expected by this coordinate operation.
+     */
+    private static final ParameterDescriptorGroup PARAMETERS;
+    static {
+        final ParameterBuilder builder = new ParameterBuilder().setRequired(true);
+        final ParameterDescriptor<?>[] parameters = {
+            createLatitude (builder.addName("Latitude of rotated pole"), true),
+            createLongitude(builder.addName("Longitude of rotated pole")),
+                            builder.addName("Axis rotation").create(Double.NaN, NonSI.DEGREE_ANGLE)
+        };
+        PARAMETERS = builder.addName("Pole rotation").createGroup(parameters);
+    }
+
+    /**
+     * Creates a new <cite>"Pole rotation"</cite> operation method.
+     */
+    public PoleRotationMock() {
+        super(2, 2, PARAMETERS);
+    }
+
+    /**
+     * Not yet supported.
+     *
+     * @param  factory    Ignored.
+     * @param  parameters Ignored.
+     * @return A dummy math transform.
+     */
+    @Override
+    public MathTransform createMathTransform(MathTransformFactory factory, ParameterValueGroup parameters) {
+        return new Unimplemented();
+    }
+
+    /**
+     * Dummy value returned by {@link PoleRotationMock#createMathTransform(MathTransformFactory, ParameterValueGroup)}.
+     */
+    private static final class Unimplemented extends AbstractMathTransform {
+        @Override public int getSourceDimensions() {return 2;}
+        @Override public int getTargetDimensions() {return 2;}
+        @Override public Matrix transform(double[] srcPts, int srcOff, double[] dstPts, int dstOff, boolean derivate) {
+            throw new UnsupportedOperationException();
+        }
+    }
+}
