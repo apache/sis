bfa844d3acae0ab06f824da7559ae7039e22bab4
Martin Desruisseaux <desruisseaux@apache.org>
2015-07-08 13:50:28 +0000

    Move LoggingWatcher in the sis-utility tests so we can use it in other tests than metadata.
    git-svn-id: https://svn.apache.org/repos/asf/sis/branches/JDK8@1689877 13f79535-47bb-0310-9956-ffa450edef68

--- core/sis-metadata/src/test/java/org/apache/sis/metadata/iso/identification/DefaultResolutionTest.java
+++ core/sis-metadata/src/test/java/org/apache/sis/metadata/iso/identification/DefaultResolutionTest.java
@@ -20,8 +20,8 @@ import javax.xml.bind.JAXBException;
 import org.opengis.metadata.identification.RepresentativeFraction;
 import org.apache.sis.xml.XML;
 import org.apache.sis.xml.Namespaces;
-import org.apache.sis.metadata.iso.LoggingWatcher;
 import org.apache.sis.internal.jaxb.Schemas;
+import org.apache.sis.test.LoggingWatcher;
 import org.apache.sis.test.DependsOn;
 import org.apache.sis.test.TestCase;
 import org.junit.Rule;
@@ -46,7 +46,7 @@ public final strictfp class DefaultResolutionTest extends TestCase {
      * details (it should have been a private field).
      */
     @Rule
-    public final LoggingWatcher listener = new LoggingWatcher() {
+    public final LoggingWatcher listener = new LoggingWatcher(DefaultResolution.LOGGER) {
         /**
          * Ensures that the logging message contains the name of the exclusive properties.
          */
