4b47347e1414c0aa6d7068744da770c534df65af
Martin Desruisseaux <desruisseaux@apache.org>
2015-09-25 08:31:59 +0000

    Move CoordinateOperationMethods.html from 'content' directory to 'book/tables' directory.
    git-svn-id: https://svn.apache.org/repos/asf/sis/branches/JDK8@1705252 13f79535-47bb-0310-9956-ffa450edef68

--- core/sis-referencing/src/test/java/org/apache/sis/internal/referencing/provider/CoordinateOperationMethodsHTML.java
+++ core/sis-referencing/src/test/java/org/apache/sis/internal/referencing/provider/CoordinateOperationMethodsHTML.java
@@ -51,7 +51,7 @@ import org.apache.sis.test.HTMLGenerator;
 
 /**
  * Generates a list of projection parameters in a HTML page. This class is used for updating the
- * <a href="http://sis.apache.org/content/CoordinateOperationMethods.html">CoordinateOperationMethods.html</a> page.
+ * <a href="http://sis.apache.org/book/tables/CoordinateOperationMethods.html">CoordinateOperationMethods.html</a> page.
  * The {@linkplain #main(String[])} method creates the "{@code CoordinateOperationMethods.html}" file in the current
  * default directory if it does not already exists. Users is responsible for moving the generated file to the Apache
  * SIS {@code "content/"} site directory.
