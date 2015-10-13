f0a20860d7941fdbc6d8337306fb1e4d0207bb70
Martin Desruisseaux <desruisseaux@apache.org>
2015-10-13 14:38:19 +0000

    More tests for ConcatenatedOperation GML marshalling.
    git-svn-id: https://svn.apache.org/repos/asf/sis/branches/JDK8@1708411 13f79535-47bb-0310-9956-ffa450edef68

--- core/sis-referencing/src/test/java/org/apache/sis/referencing/operation/DefaultConcatenatedOperationTest.java
+++ core/sis-referencing/src/test/java/org/apache/sis/referencing/operation/DefaultConcatenatedOperationTest.java
@@ -16,13 +16,16 @@
  */
 package org.apache.sis.referencing.operation;
 
-import org.opengis.test.Validators;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
 import javax.xml.bind.JAXBException;
+import org.opengis.referencing.operation.CoordinateOperation;
+import org.opengis.test.Validators;
 import org.apache.sis.test.DependsOn;
 import org.apache.sis.test.XMLTestCase;
 import org.junit.Test;
 
-import static org.apache.sis.test.ReferencingAssert.*;
+import static org.opengis.test.Assert.*;
+import static org.apache.sis.test.TestUtilities.getSingleton;
 
 
 /**
@@ -43,7 +46,6 @@ public class DefaultConcatenatedOperationTest extends XMLTestCase {
      */
     private static final String XML_FILE = "ConcatenatedOperation.xml";
 
-
     /**
      * Tests (un)marshalling of a concatenated operation.
      *
@@ -53,10 +55,23 @@ public class DefaultConcatenatedOperationTest extends XMLTestCase {
     public void testXML() throws JAXBException {
         final DefaultConcatenatedOperation op = unmarshalFile(DefaultConcatenatedOperation.class, XML_FILE);
         Validators.validate(op);
-        if (false) assertEpsgNameAndIdentifierEqual("WGS 84", 4979, op);
+        assertEquals("operations.size()", 2, op.getOperations().size());
+        final CoordinateOperation step1 = op.getOperations().get(0);
+        final CoordinateOperation step2 = op.getOperations().get(1);
+        final CoordinateReferenceSystem sourceCRS = op.getSourceCRS();
+        final CoordinateReferenceSystem targetCRS = op.getTargetCRS();
+
+        assertIdentifierEquals(          "identifier", "test", "test", null, "concatenated", getSingleton(op       .getIdentifiers()));
+        assertIdentifierEquals("sourceCRS.identifier", "test", "test", null, "source",       getSingleton(sourceCRS.getIdentifiers()));
+        assertIdentifierEquals("targetCRS.identifier", "test", "test", null, "target",       getSingleton(targetCRS.getIdentifiers()));
+        assertIdentifierEquals(    "step1.identifier", "test", "test", null, "step-1",       getSingleton(step1    .getIdentifiers()));
+        assertIdentifierEquals(    "step2.identifier", "test", "test", null, "step-2",       getSingleton(step2    .getIdentifiers()));
+        assertSame("tmp CRS",   step1.getTargetCRS(), step2.getSourceCRS());
+        assertSame("sourceCRS", step1.getSourceCRS(), sourceCRS);
+        assertSame("targetCRS", step2.getTargetCRS(), targetCRS);
         /*
          * Test marshalling and compare with the original file.
          */
-//        assertMarshalEqualsFile(XML_FILE, op, "xmlns:*", "xsi:schemaLocation");
+        assertMarshalEqualsFile(XML_FILE, op, "xmlns:*", "xsi:schemaLocation");
     }
 }
