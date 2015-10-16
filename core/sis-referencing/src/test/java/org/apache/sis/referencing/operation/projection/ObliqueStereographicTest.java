f4b31f85bd0d5d036a2426b0b85f8686c8a08de0
Rémi Maréchal <rmarechal@apache.org>
2015-10-16 16:13:34 +0000

    Projection : Add ObliqueStereographic spherical case and relative tests
    git-svn-id: https://svn.apache.org/repos/asf/sis/branches/JDK8@1709049 13f79535-47bb-0310-9956-ffa450edef68

--- core/sis-referencing/src/test/java/org/apache/sis/referencing/operation/projection/ObliqueStereographicTest.java
+++ core/sis-referencing/src/test/java/org/apache/sis/referencing/operation/projection/ObliqueStereographicTest.java
@@ -30,6 +30,10 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import static java.lang.Math.sqrt;
+import static java.lang.StrictMath.toRadians;
+import org.apache.sis.referencing.operation.matrix.Matrix2;
+import org.apache.sis.test.DependsOnMethod;
+import org.opengis.referencing.operation.Matrix;
 
 
 /**
@@ -42,6 +46,12 @@ import static java.lang.Math.sqrt;
  * @module
  */
 public final strictfp class ObliqueStereographicTest extends MapProjectionTestCase {
+    /**
+     * Tolerance used to compare spherical and oblique formula
+     * into same parameters context.
+     */
+    private final static double EPSG_SPHERICAL_TOLERANCE = 1E-15;
+
     /**
      * Parameter values provided by the <a href = http://www.iogp.org/pubs/373-07-2.pdf>EPSG guide</a>
      * for testing {@link ObliqueStereographic} transform conformity.
@@ -65,30 +75,62 @@ public final strictfp class ObliqueStereographicTest extends MapProjectionTestCa
                                 R        = 6382644.571 / a;
 
     /**
-     * Tested {@link MathTransform} projection.
+     * Tested {@link ObliqueStereographic} projection.
      */
-    private final NormalizedProjection ObliqueStereographic;
+    private final ObliqueStereographic obliqueStereographic;
+
+    /**
+     * {@link ObliqueStereographic} projection define into spherical particularity case.
+     * Used to compute the expected transform, invert transform and derivative values
+     * when the projection is spherical.
+     */
+    private final ObliqueStereographic obliqueReference;
+
+    /**
+     * Tested {@link ObliqueStereographic} projection into spherical particularity case.
+     */
+    private final ObliqueStereographic.Spherical sphericalObliqueStereographic;
 
     /**
      * Buid tested {@link ObliqueStereographic} {@link MathTransform}.
      */
     public ObliqueStereographicTest() {
+
+        //-- oblique EPSG case
         final OperationMethod op = new org.apache.sis.internal.referencing.provider.ObliqueStereographic();
 
-        final ParameterValueGroup p = op.getParameters().createValue();
+        final ParameterValueGroup obliqueParameters = op.getParameters().createValue();
 
         //-- implicit names from OGC.
-        p.parameter("semi_major").setValue(6377397.155);
-        p.parameter("inverse_flattening").setValue(299.15281);
+        obliqueParameters.parameter("semi_major").setValue(6377397.155);
+        obliqueParameters.parameter("inverse_flattening").setValue(299.15281);
 
         //-- Name parameters from Epsg registry
-        p.parameter("Latitude of natural origin").setValue(52.156160556);
-        p.parameter("Longitude of natural origin").setValue(5.387638889);
-        p.parameter("Scale factor at natural origin").setValue(0.9999079);
-        p.parameter("False easting").setValue(155000.00);
-        p.parameter("False northing").setValue(463000.00);
+        obliqueParameters.parameter("Latitude of natural origin").setValue(52.156160556);
+        obliqueParameters.parameter("Longitude of natural origin").setValue(5.387638889);
+        obliqueParameters.parameter("Scale factor at natural origin").setValue(0.9999079);
+        obliqueParameters.parameter("False easting").setValue(155000.00);
+        obliqueParameters.parameter("False northing").setValue(463000.00);
 
-        ObliqueStereographic = new ObliqueStereographic(op, (Parameters) p);
+        obliqueStereographic = new ObliqueStereographic(op, (Parameters) obliqueParameters);
+
+
+        //-- spherical case
+        final ParameterValueGroup sphericalParameters = op.getParameters().createValue();
+
+        //-- implicit names from OGC.
+        sphericalParameters.parameter("semi_major").setValue(6377397.155);
+        sphericalParameters.parameter("semi_minor").setValue(6377397.155);
+
+        //-- Name parameters from Epsg registry
+        sphericalParameters.parameter("Latitude of natural origin").setValue(52.156160556);
+        sphericalParameters.parameter("Longitude of natural origin").setValue(5.387638889);
+        sphericalParameters.parameter("Scale factor at natural origin").setValue(0.9999079);
+        sphericalParameters.parameter("False easting").setValue(155000.00);
+        sphericalParameters.parameter("False northing").setValue(463000.00);
+
+        obliqueReference              = new ObliqueStereographic(op, (Parameters) sphericalParameters);
+        sphericalObliqueStereographic = new ObliqueStereographic.Spherical(obliqueReference);
     }
 
     /**
@@ -110,7 +152,7 @@ public final strictfp class ObliqueStereographicTest extends MapProjectionTestCa
 
         srcPts[0] = srcPts[0] * n;
 
-        ObliqueStereographic.transform(srcPts, 0, dstPts, 0, 1);
+        obliqueStereographic.transform(srcPts, 0, dstPts, 0, 1);
 
         final double destE = dstPts[0] * k0 * a * 2 * R + FE;
         final double destN = dstPts[1] * k0 * a * 2 * R + FN;
@@ -146,7 +188,7 @@ public final strictfp class ObliqueStereographicTest extends MapProjectionTestCa
         final double[] srcPts = new double[]{srcEast, srcNorth}; //-- meter
 
         final double[] dstPts = new double[2];
-        ObliqueStereographic.inverseTransform(srcPts, 0, dstPts, 0);
+        obliqueStereographic.inverseTransform(srcPts, 0, dstPts, 0);
 
         final double λO = 0.094032038;
 
@@ -170,7 +212,129 @@ public final strictfp class ObliqueStereographicTest extends MapProjectionTestCa
      * @see org.opengis.test.referencing.ParameterizedTransformTest#testObliqueStereographic()
      */
     @Test
-    public void testGeoapi() throws FactoryException, TransformException {
+    public void testObliqueStereographic() throws FactoryException, TransformException {
         createGeoApiTest(new org.apache.sis.internal.referencing.provider.ObliqueStereographic()).testObliqueStereographic();
+    }
+
+    /**
+     * Verifies the consistency of elliptical formulas with the spherical formulas.
+     * This test compares the results between elliptical and spherical transform formulas
+     * from the particularity case stipulated into EPSG guide.
+     *
+     * @see ObliqueStereographic.Spherical#transform(double[], int, double[], int, boolean)
+     * with boolean setted to {@code false}
+     * @throws TransformException if an error occurred while projecting a coordinate.
+     */
+    @Test
+    @DependsOnMethod("testObliqueStereographic")
+    public void EPSGEllipticalWithSphericalTransform() throws TransformException {
+
+        final double[] srcPts = new double[]{6, 53}; //-- deg
+        srcPts[0] = Math.toRadians(srcPts[0] - 5.387638889) ;
+        srcPts[1] = Math.toRadians(srcPts[1]);
+
+        final double[] dstPts = new double[2];
+
+        srcPts[0] = srcPts[0] * n;
+
+        obliqueReference.transform(srcPts, 0, dstPts, 0, 1);
+
+        final double destE = dstPts[0] * k0 * a * 2 * R + FE;
+        final double destN = dstPts[1] * k0 * a * 2 * R + FN;
+
+        sphericalObliqueStereographic.transform(srcPts, 0, dstPts, 0, 1);
+
+        final double destSphereE = dstPts[0] * k0 * a * 2 * R + FE;
+        final double destSphereN = dstPts[1] * k0 * a * 2 * R + FN;
+
+        Assert.assertEquals("destination East coordinate",  destE, destSphereE, EPSG_SPHERICAL_TOLERANCE);
+        Assert.assertEquals("destination North coordinate", destN, destSphereN, EPSG_SPHERICAL_TOLERANCE);
+    }
+
+    /**
+     * Verifies the consistency of elliptical formulas with the spherical formulas.
+     * This test compares the results between elliptical and spherical invert transform formulas
+     * from the particularity case stipulated into EPSG guide.
+     *
+     * @see ObliqueStereographic.Spherical#inverseTransform(double[], int, double[], int)
+     * @throws ProjectionException if an error occurred while invert transformation computing.
+     */
+    @Test
+    @DependsOnMethod("testObliqueStereographic")
+    public void EPSGEllipticalWithSphericalInvertTransform() throws ProjectionException {
+        double srcEast  = 196105.28;
+        double srcNorth = 557057.74;
+
+        srcEast  -= FE;
+        srcNorth -= FN;
+        srcEast  /= k0;
+        srcNorth /= k0;
+        srcEast  /= a;
+        srcNorth /= a;
+        srcEast  /= (2 * R);
+        srcNorth /= (2 * R);
+
+        //-- tcheck transform
+        final double[] srcProjPts = new double[]{srcEast, srcNorth}; //-- meter
+
+        final double[] dstPts = new double[2];
+
+        obliqueReference.inverseTransform(srcProjPts, 0, dstPts, 0);
+
+        final double destλ = dstPts[0];
+        final double destφ = dstPts[1];
+
+        sphericalObliqueStereographic.inverseTransform(srcProjPts, 0, dstPts, 0);
+
+        final double destSphereλ = dstPts[0];
+        final double destSphereφ = dstPts[1];
+
+        Assert.assertEquals("destination East coordinate",  destλ, destSphereλ, EPSG_SPHERICAL_TOLERANCE);
+        Assert.assertEquals("destination North coordinate", destφ, destSphereφ, EPSG_SPHERICAL_TOLERANCE);
+    }
+
+    /**
+     * Verifies the consistency of elliptical formulas with the spherical formulas.
+     * This test compares the results between elliptical and spherical derivative formulas
+     * from the particularity case stipulated into EPSG guide.
+     *
+     * @see ObliqueStereographic.Spherical#transform(double[], int, double[], int, boolean)
+     * with boolean setted to {@code true}
+     * @throws ProjectionException if an error occurred while derivative computing.
+     */
+    @Test
+    @DependsOnMethod("testObliqueStereographic")
+    public void EPSGEllipticalWithSphericalDerivative() throws ProjectionException {
+
+        final Matrix2 derivativeTolerance = new Matrix2(EPSG_SPHERICAL_TOLERANCE, EPSG_SPHERICAL_TOLERANCE,
+                                                                                   EPSG_SPHERICAL_TOLERANCE, EPSG_SPHERICAL_TOLERANCE);
+        final double[] srcPts = new double[]{6, 53}; //-- deg
+        srcPts[0] = Math.toRadians(srcPts[0] - 5.387638889) ;
+        srcPts[1] = Math.toRadians(srcPts[1]);
+
+        srcPts[0] = srcPts[0] * n;
+
+        //-- Derivative
+        final Matrix expectedDerivative = obliqueReference.transform(srcPts, 0, null, 0, true);
+        final Matrix testedDerivative   = sphericalObliqueStereographic.transform(srcPts, 0, null, 0, true);
+
+        assertMatrixEquals("derivative spherical", expectedDerivative, testedDerivative, derivativeTolerance);
+    }
+
+    /**
+     * Creates a projection and derivates a few points.
+     *
+     * @throws TransformException Should never happen.
+     */
+    @Test
+    public void testSphericalDerivative() throws TransformException {
+        transform = obliqueStereographic;
+        tolerance = 1E-9;
+
+        final double delta = toRadians(100.0 / 60) / 1852; // Approximatively 100 metres.
+        derivativeDeltas = new double[] {delta, delta};
+        verifyDerivative(toRadians( 0), toRadians( 0));
+        verifyDerivative(toRadians(-3), toRadians(30));
+        verifyDerivative(toRadians(+6), toRadians(60));
     }
 }
