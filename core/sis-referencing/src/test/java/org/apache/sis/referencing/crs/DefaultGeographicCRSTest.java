7a3ff98f159f813e81819d97cb7913eb335cf28b
Martin Desruisseaux <desruisseaux@apache.org>
2016-02-25 18:16:26 +0000

    When parsing a WKT 1 string, convert (Geographic2D + Vertical) into (Geographic3D) CRS. Issue: SIS-317.
    git-svn-id: https://svn.apache.org/repos/asf/sis/branches/JDK8@1732336 13f79535-47bb-0310-9956-ffa450edef68

--- core/sis-referencing/src/test/java/org/apache/sis/referencing/crs/DefaultGeographicCRSTest.java
+++ core/sis-referencing/src/test/java/org/apache/sis/referencing/crs/DefaultGeographicCRSTest.java
@@ -172,9 +172,9 @@ public final strictfp class DefaultGeographicCRSTest extends TestCase {
      * <p>This CRS used in this test is equivalent to {@code EPSG:4979} except for axis order,
      * since EPSG puts latitude before longitude.</p>
      *
-     * @since 0.7
-     *
      * @see #testWKT1_For3D()
+     *
+     * @since 0.7
      */
     @Test
     @DependsOnMethod("testWKT2")
@@ -300,9 +300,10 @@ public final strictfp class DefaultGeographicCRSTest extends TestCase {
      * geographic CRS followed by an ellipsoidal height. Such construction is illegal according ISO 19111, so this
      * split shall be done on-the-fly only for formatting purpose.
      *
-     * @since 0.7
-     *
      * @see #testWKT2_For3D()
+     * @see <a href="https://issues.apache.org/jira/browse/SIS-317">SIS-317</a>
+     *
+     * @since 0.7
      */
     @Test
     @DependsOnMethod("testWKT1")
