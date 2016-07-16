fea0ed79f5414815b6d9ceb7e36ee750b30dd242
desruisseaux <martin.desruisseaux@geomatys.fr>
2016-07-17 00:14:20 +0200

    Restore the SummaryRecord(Metadata) constructor.

--- application/sis-webapp/src/main/java/org/apache/sis/services/csw/SummaryRecord.java
+++ application/sis-webapp/src/main/java/org/apache/sis/services/csw/SummaryRecord.java
@@ -16,12 +16,35 @@
  */
 package org.apache.sis.services.csw;
 
+import java.util.Collection;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Locale;
+import java.util.Set;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
+import org.opengis.util.InternationalString;
+import org.opengis.metadata.Identifier;
+import org.opengis.metadata.Metadata;
+import org.opengis.metadata.MetadataScope;
+import org.opengis.metadata.citation.Citation;
+import org.opengis.metadata.citation.CitationDate;
+import org.opengis.metadata.citation.DateType;
+import org.opengis.metadata.distribution.Distribution;
+import org.opengis.metadata.distribution.Format;
+import org.opengis.metadata.extent.Extent;
+import org.opengis.metadata.extent.GeographicBoundingBox;
+import org.opengis.metadata.extent.GeographicExtent;
+import org.opengis.metadata.identification.Identification;
+import org.opengis.metadata.maintenance.ScopeCode;
+import org.apache.sis.metadata.iso.extent.DefaultGeographicBoundingBox;
+import org.apache.sis.referencing.IdentifiedObjects;
+import org.apache.sis.util.iso.Types;
+
 
 /**
  * Summary of an ISO 19115 metadata record. The summary is composed in part from Dublin Core elements.
@@ -142,7 +165,153 @@ public class SummaryRecord {
     @XmlElement(namespace = Element.OWS)
     private BoundingBox BoundingBox;
 
-    public SummaryRecord() {
+    /**
+     * Creates an initially empty summary record.
+     * This constructor is invoked by JAXB at unmarshalling time.
+     */
+    SummaryRecord() {
+    }
+
+    /**
+     * Creates a summary record initialized to the values extracted from the given ISO 19115 metadata.
+     * This constructor is invoked before marshalling with JAXB.
+     *
+     * @param metadata  the root ISO 19115 metadata instance where to get information.
+     * @param locale    the locale to use for converting {@link InternationalString} to {@link String},
+     *                  or {@code null} for the system default.
+     */
+    public SummaryRecord(final Metadata metadata, final Locale locale) {
+        /*
+         * Get identifier and date information from the root metadata object. Note that:
+         *
+         *   - Identifier is optional in ISO 19115 but mandatory in <csw:Record>.
+         *   - Date information is mandatory in ISO 19115.
+         *
+         * First, try to get those information from the paths specified by OGC 07-045.
+         * They should be there, but if for some reason those information are missing,
+         * then the loop below will search for fallbacks in resource citations.
+         */
+        identifier = IdentifiedObjects.toString(metadata.getMetadataIdentifier());              // May be null.
+        setModified(metadata.getDateInfo());
+        /*
+         * Collect all titles, ignoring duplicated values. Opportunistically search for
+         * dates and identifier to use as fallbacks if the above code did not found them.
+         * Those fallbacks are specific to Apache SIS (not part of OGC 07-045).
+         */
+        GeographicBoundingBox bbox = null;
+        DefaultGeographicBoundingBox union = null;
+        final Set<String> titles = new HashSet<>();
+        for (final Identification identification : metadata.getIdentificationInfo()) {
+            if (identification != null) {
+                final Citation citation = identification.getCitation();
+                if (citation != null) {
+                    final InternationalString i18n = citation.getTitle();
+                    if (i18n != null) {
+                        titles.add(i18n.toString(locale));
+                    }
+                    if (identifier == null) {
+                        for (final Identifier id : citation.getIdentifiers()) {
+                            identifier = IdentifiedObjects.toString(id);
+                            if (identifier != null) break;                      // Stop at the first identifier.
+                        }
+                    }
+                    if (modified == null) {
+                        setModified(citation.getDates());
+                    }
+                }
+                for (final Extent extent : identification.getExtents()) {
+                    for (final GeographicExtent geo : extent.getGeographicElements()) {
+                        if (geo instanceof GeographicBoundingBox) {
+                            if (bbox == null) {
+                                bbox = (GeographicBoundingBox) geo;
+                            } else {
+                                if (union == null) {
+                                    bbox = union = new DefaultGeographicBoundingBox(bbox);
+                                }
+                                union.add((GeographicBoundingBox) geo);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if (bbox != null) {
+            BoundingBox = new BoundingBox(bbox);
+        }
+        title = toString(titles, System.lineSeparator());
+        titles.clear();
+
+        // Collect all formats, ignoring duplicated values.
+        for (final Distribution distribution : metadata.getDistributionInfo()) {
+            for (final Format df : distribution.getDistributionFormats()) {
+                final Citation citation = df.getFormatSpecificationCitation();
+                if (citation != null) {
+                    for (final InternationalString i18n : citation.getAlternateTitles()) {
+                        if (i18n != null) {
+                            titles.add(i18n.toString(locale));
+                        }
+                    }
+                }
+            }
+        }
+        format = toString(titles, ", ");
+        titles.clear();
+
+        // Retain only the first type, if any.
+        ScopeCode code = null;
+        for (final MetadataScope scope : metadata.getMetadataScopes()) {
+            code = scope.getResourceScope();
+            if (code != null) break;
+        }
+        if (code == null) {
+            code = ScopeCode.DATASET;       // Default value specified by OGC 07-045.
+        }
+        type = Types.getCodeName(code);
+    }
+
+    /**
+     * Sets the {@link #modified} field to the latest {@code CREATION} or {@code LAST_UPDATE} date
+     * found in the given collection. If the given is null or empty, then this method does nothing.
+     */
+    private void setModified(final Collection<? extends CitationDate> dates) {
+        if (dates != null) {                            // Paranoiac check.
+            for (final CitationDate date : dates) {
+                final DateType dt = date.getDateType();
+                if (DateType.CREATION.equals(dt) || DateType.LAST_UPDATE.equals(dt)) {
+                    final Date t = date.getDate();
+                    if (t != null) {
+                        if (modified == null || t.after(modified)) {
+                            modified = t;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Formats the elements in the given set as a list separated by the given string.
+     * This method avoid to create a temporary buffer when the set contains 0 or 1 elements.
+     *
+     * @param  items      the items to format as a list.
+     * @param  separator  the separator to put between items (comma or new line).
+     * @return the given set formatted as a list.
+     */
+    private static String toString(final Set<String> items, final String separator) {
+        items.remove(null);         // Safety in case the user metadata contains null elements.
+        final Iterator<String> it = items.iterator();
+        if (!it.hasNext()) {
+            return null;
+        }
+        final String first = it.next();
+        if (!it.hasNext()) {
+            return first;            // Most common case: only one item in the set.
+        }
+        final StringBuilder buffer = new StringBuilder(first);
+        while (it.hasNext()) {
+            buffer.append(separator).append(it.next());
+        }
+        return buffer.toString();
     }
 
     /**
