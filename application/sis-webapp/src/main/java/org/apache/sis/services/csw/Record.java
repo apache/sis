e9c19b157fa34b56abb4c52f0bbc12706588acdd
haonguyen123 <nguyenthiphuonghao243@gmail.com>
2016-07-19 01:13:46 +0700

    reupdate Record

--- application/sis-webapp/src/main/java/org/apache/sis/services/csw/Record.java
+++ application/sis-webapp/src/main/java/org/apache/sis/services/csw/Record.java
@@ -45,6 +45,7 @@ import org.opengis.metadata.identification.Identification;
  */
 public class Record {
     
+    
     Map<String, SummaryRecord> record;
     /**
      * Contructor's Record
@@ -58,39 +59,26 @@ public class Record {
      * outside the method or constructor boundary. 
      */
     public Record() throws IOException, DataStoreException, Exception {
-        ConfigurationReader path = new ConfigurationReader();
-        File directory = new File(path.getPropValues());
+        Catalog catalog = new Catalog();
         record = new HashMap();
         /**
          * Get all the files from a directory.
          *
          */
-        File[] fList = directory.listFiles();
-        for (File file : fList) {
-            final Metadata md;
-            if (file.isFile() && file.getName().endsWith(".txt")) {
-
-                try (BufferedReader in = new BufferedReader(new FileReader(file.getPath()))) {
-                    final LandsatReader reader = new LandsatReader(in);
-                    md = reader.read();
-                }
-            } else if (file.isFile() && file.getName().endsWith(".xml")) {
-                File xml = new File(file.getPath());
-                final ModisReader read = new ModisReader(xml);
-                md = read.read();
-            } else {
-                continue;
-            }
-            String key = md.getFileIdentifier();
-            SummaryRecord summary = new SummaryRecord(md);
-            record.put(key, summary);
+        for (Metadata id : catalog.getAllMetadata()) {
+            SummaryRecord summary = new SummaryRecord(id);
+            String key = id.getFileIdentifier();
+            record.put(key,  summary);
         }
     }
     /**
      * Return all record of metadata
      *
-     * @return
-     * @throws Exception
+     * @return all record of metadata
+     * @throws Exception  Exception checked exceptions. Checked exceptions need to be
+     * declared in a method or constructor's {@code throws} clause if they can
+     * be thrown by the execution of the method or constructor and propagate
+     * outside the method or constructor boundary.
      */
     public List<SummaryRecord> getAllRecord() { 
         return new ArrayList<SummaryRecord>(record.values()); 
@@ -99,10 +87,13 @@ public class Record {
     /**
      * Return All Record Paginated
      *
-     * @param start
-     * @param size
-     * @return
-     * @throws Exception
+     * @param start start record 
+     * @param size size list record 
+     * @return All Record Paginated
+     * @throws Exception checked exceptions. Checked exceptions need to be
+     * declared in a method or constructor's {@code throws} clause if they can
+     * be thrown by the execution of the method or constructor and propagate
+     * outside the method or constructor boundary.
      */
     public List<SummaryRecord> getAllRecordPaginated(int start, int size) throws Exception {
         ArrayList<SummaryRecord> list = new ArrayList<SummaryRecord>(record.values()); 
@@ -115,11 +106,16 @@ public class Record {
     /**
      * Return record by id
      *
-     * @param id
+     * @param id identifier for record .
      * @return Record with id = identifier.
-     * @throws Exception
+     * @return Record with id = identifier.
+     * @throws Exception checked exceptions. Checked exceptions need to be
+     * declared in a method or constructor's {@code throws} clause if they can
+     * be thrown by the execution of the method or constructor and propagate
+     * outside the method or constructor boundary.
      */
     public SummaryRecord getRecordById(String id) throws Exception {
         return record.get(id);
     }
+    
 }
