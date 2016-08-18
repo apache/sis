0cbecabe156be9c0176f7f85110f13a720e1c93e
Johann Sorel <jsorel@apache.org>
2016-08-18 12:49:48 +0000

    Fix javadoc error
    git-svn-id: https://svn.apache.org/repos/asf/sis/branches/JDK8@1756772 13f79535-47bb-0310-9956-ffa450edef68

--- core/sis-utility/src/main/java/org/apache/sis/internal/converter/SystemRegistry.java
+++ core/sis-utility/src/main/java/org/apache/sis/internal/converter/SystemRegistry.java
@@ -32,7 +32,7 @@ import org.apache.sis.internal.system.Modules;
  *
  * <ul>
  *   <li>Fetch the list of converters from the content of all
- *       {@code META-INF/services/org.apache.sis.util.converter.ObjectConverter} files found on the classpath.
+ *       {@code META-INF/services/org.apache.sis.util.ObjectConverter} files found on the classpath.
  *       The intend is to allow other modules to register their own converters.</li>
  *
  *   <li>Apply heuristic rules in addition to the explicitly registered converters.
