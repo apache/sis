744c0ba701d09a435e93abe0f3c32e16496ab8f6
Martin Desruisseaux <desruisseaux@apache.org>
2016-08-30 12:44:02 +0000

    Expand the number of fraction digits in second from 1 to 3, in order to avoid an ambiguity in the way SimpleDateFormat interpret the "S" pattern in "ss.S".
    git-svn-id: https://svn.apache.org/repos/asf/sis/branches/JDK8@1758380 13f79535-47bb-0310-9956-ffa450edef68

--- core/sis-utility/src/main/java/org/apache/sis/internal/util/StandardDateFormat.java
+++ core/sis-utility/src/main/java/org/apache/sis/internal/util/StandardDateFormat.java
@@ -19,12 +19,13 @@ package org.apache.sis.internal.util;
 import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
+import java.text.FieldPosition;
 import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
 
 
 /**
- * A date format used for parsing date in the {@code "yyyy-MM-dd'T'HH:mm:ss.SX"} pattern, but in which
+ * A date format used for parsing date in the {@code "yyyy-MM-dd'T'HH:mm:ss.SSSX"} pattern, but in which
  * the time is optional. The "Apache SIS for JDK8" branch can use the {@link java.time.format} package,
  * while other branches use {@link java.text.SimpleDateFormat}.
  *
@@ -33,20 +34,14 @@ import java.text.SimpleDateFormat;
  *
  * @author  Martin Desruisseaux (Geomatys)
  * @since   0.6
- * @version 0.6
+ * @version 0.8
  * @module
  */
 public final class StandardDateFormat extends SimpleDateFormat {
     /**
-     * For compatibility between versions of a same branch. The Apache SIS form JDK8 and for JDK7 branches
-     * may have incompatible classes, and consequently different serial version UID.
+     * For cross-version compatibility.
      */
     private static final long serialVersionUID = 1552761359761440473L;
-
-    /**
-     * The pattern of dates.
-     */
-    public static final String PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SX";
 
     /**
      * Short version of {@link #PATTERN}, to be used when formatting temporal extents
@@ -58,6 +53,30 @@ public final class StandardDateFormat extends SimpleDateFormat {
     public static final String SHORT_PATTERN = "yyyy-MM-dd";
 
     /**
+     * The pattern of time. We use 3 fraction digits for the seconds because {@code SimpleDateFormat} parses the
+     * milliseconds as an integer instead than as fraction digits. For example with 1 fraction digits, "00:00:01.4"
+     * is parsed as 1.004 seconds instead of 1.4. While surprising, this is conform to the {@code SimpleDateFormat}
+     * specification. Note that this is different than {@link java.time.LocalDateTime} which parse those numbers as
+     * fraction digits.
+     */
+    public static final String TIME_PATTERN = "HH:mm:ss.SSSX";
+
+    /**
+     * Number of fraction digits in {@link #TIME_PATTERN}.
+     */
+    private static final int NUM_FRACTION_DIGITS = 3;
+
+    /**
+     * The pattern of dates.
+     */
+    public static final String PATTERN = SHORT_PATTERN + "'T'" + TIME_PATTERN;
+
+    /**
+     * {@code false} if this {@code StandardDateFormat} object is parsing only the days, without time.
+     */
+    private boolean hasTime;
+
+    /**
      * Creates a new format for a default locale in the UTC timezone.
      */
     public StandardDateFormat() {
@@ -67,7 +86,7 @@ public final class StandardDateFormat extends SimpleDateFormat {
     /**
      * Creates a new format for the given locale in the UTC timezone.
      *
-     * @param locale The locale of the format to create.
+     * @param locale  the locale of the format to create.
      */
     public StandardDateFormat(final Locale locale) {
         this(locale, TimeZone.getTimeZone("UTC"));
@@ -76,44 +95,178 @@ public final class StandardDateFormat extends SimpleDateFormat {
     /**
      * Creates a new format for the given locale.
      *
-     * @param locale The locale of the format to create.
-     * @param timezone The timezone.
+     * @param locale    the locale of the format to create.
+     * @param timezone  the timezone.
      */
     public StandardDateFormat(final Locale locale, final TimeZone timezone) {
         super(PATTERN, locale);
-        setTimeZone(timezone);
+        super.setTimeZone(timezone);
+        hasTime = true;
+    }
+
+    /**
+     * Formats the given date.
+     *
+     * @param  date    the date to format.
+     * @param  buffer  where to format the date.
+     * @param  pos     where to store information about a date field.
+     * @return the given buffer, for method calls chaining.
+     */
+    @Override
+    public StringBuffer format(final Date date, final StringBuffer buffer, final FieldPosition pos) {
+        if (!hasTime) {
+            applyPattern(PATTERN);
+            hasTime = true;
+        }
+        return super.format(date, buffer, pos);
     }
 
     /**
      * Parses the given text starting at the given position.
      *
-     * @param  text The text to parse.
-     * @param  position Position where to start the parsing.
-     * @return The date, or {@code null} if we failed to parse it.
+     * @param  text      the text to parse.
+     * @param  position  position where to start the parsing.
+     * @return the date, or {@code null} if we failed to parse it.
      */
     @Override
     public Date parse(final String text, final ParsePosition position) {
-        Date date = super.parse(text, position);
-        if (date == null) {
-            /*
-             * The "yyyy-MM-dd'T'HH:mm:ss.SX" pattern may fail if the user did not specified the time part.
-             * So if we fail to parse using the full pattern, try again with only the "yyyy-MM-dd" pattern.
-             */
-            final String pattern = toPattern();
-            if (pattern.startsWith(SHORT_PATTERN)) {
-                final int errorIndex = position.getErrorIndex();
-                position.setErrorIndex(-1);
+        final Fix fix = Fix.apply(text, position.getIndex(), 0);
+        if (fix == null) {
+            if (hasTime) {
                 applyPattern(SHORT_PATTERN);
-                try {
-                    date = parse(text, position);
-                } finally {
-                    applyPattern(pattern);
-                }
-                if (date == null) {
-                    position.setErrorIndex(errorIndex); // Reset original error index.
-                }
+                hasTime = false;
             }
+            return super.parse(text, position);
         }
+        if (!hasTime) {
+            applyPattern(PATTERN);
+            hasTime = true;
+        }
+        final Date date = super.parse(fix.text, position);
+        position.setIndex     (fix.adjustIndex(position.getIndex()));
+        position.setErrorIndex(fix.adjustIndex(position.getErrorIndex()));
         return date;
+    }
+
+    /**
+     * Modifies if needed a given input string in order to make it compliant with JDK7 implementation of
+     * {@code SimpleDateFormat}. That implementation expects the exact some number of fraction digits in
+     * the second fields than specified by the {@code "ss.SSS"} part of the pattern. This method adds or
+     * removes fraction digits as needed.
+     */
+    static final class Fix {
+        /**
+         * The modified input string, with second fraction digits added or removed.
+         */
+        public final String text;
+
+        /**
+         * Index of the first character added or removed, or {@code input.length()} if none.
+         */
+        private final int lower;
+
+        /**
+         * Number of characters added (positive number) or removed (negative number).
+         */
+        final int change;
+
+        /**
+         * Wraps information about an input string that has been made parsable.
+         */
+        private Fix(final String text, final int lower, final int change) {
+            this.text   = text;
+            this.lower  = lower;
+            this.change = change;
+        }
+
+        /**
+         * Performs various adjustments for making the given text compliant with the format expected by
+         * a {@link SimpleDateFormat} using the {@value #PATTERN} pattern.
+         *
+         * @param  text       the text to adapt.
+         * @param  s          index in {@code text} where to start the parsing.
+         * @param  timeField  0 if parsing starts on days, 1 if starting on hours field,
+         *                    2 if starting on minutes field or 3 if starting on seconds field.
+         * @return information about the input string made parsable,
+         *         or {@code null} if the given text does not contain a time field.
+         */
+        static Fix apply(final String text, int s, int timeField) {
+            final int length = text.length();
+search:     while (s < length) {
+                char c = text.charAt(s);
+                if (c < '0' || c > '9') {
+                    switch (c) {
+                        default: {
+                            break search;
+                        }
+                        case '-': {
+                            if (timeField != 0) break search;
+                            break;
+                        }
+                        case 'T': {
+                            if (timeField != 0) break search;
+                            timeField = 1;
+                            break;
+                        }
+                        case ':': {
+                            if (timeField == 0 || ++timeField > 3) break search;
+                            break;
+                        }
+                        case '.': {
+                            if (timeField != 3) break search;
+                            /*
+                             * If the user specified too few or too many fraction digits, add or truncate.
+                             * If the number of digits is already the expected ones (which should be rare),
+                             * nevertheless create a new Fix instance for notifying the parse method that
+                             * the text contains a time.
+                             */
+                            final int start = ++s;
+                            while (s < length && (c = text.charAt(s)) >= '0' && c <= '9') s++;
+                            final int change = NUM_FRACTION_DIGITS - (s - start);
+                            if (change == 0) {
+                                return new Fix(text, length, 0);                    // See above comment.
+                            }
+                            final StringBuilder buffer = new StringBuilder(text);
+                            if (change >= 0) {
+                                for (int i = change; --i >= 0;) {
+                                    buffer.insert(s, '0');
+                                }
+                            } else {
+                                final int upper = s;
+                                s = start + NUM_FRACTION_DIGITS;
+                                buffer.delete(s, upper);
+                            }
+                            return new Fix(buffer.toString(), s, change);
+                        }
+                    }
+                }
+                s++;
+            }
+            /*
+             * If the user did not specified any fraction digits, add them.
+             * (NUM_FRACTION_DIGITS + 1) shall be the length of the inserted string.
+             */
+            final String time;
+            switch (timeField) {
+                default: return null;
+                case 1:  time = ":00:00.000"; break;
+                case 2:  time =    ":00.000"; break;
+                case 3:  time =       ".000"; break;
+            }
+            return new Fix(new StringBuilder(text).insert(s, time).toString(), s, time.length());
+        }
+
+        /**
+         * Map an index in the modified string to the index in the original string.
+         *
+         * @param  index  the index in the modified string.
+         * @return the corresponding index in the original string.
+         */
+        int adjustIndex(int index) {
+            if (index >= lower) {
+                index = Math.max(lower, index - change);
+            }
+            return index;
+        }
     }
 }
