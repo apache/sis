f3470c14b03a135022a98fe62a74a7e1e1ea63ae
Martin Desruisseaux <desruisseaux@apache.org>
2016-09-29 08:46:31 +0000

    Added comments advising to not use those internal resources outside the sis-referencing module.
    git-svn-id: https://svn.apache.org/repos/asf/sis/branches/JDK8@1762746 13f79535-47bb-0310-9956-ffa450edef68

--- core/sis-referencing/src/main/java/org/apache/sis/internal/referencing/Resources.java
+++ core/sis-referencing/src/main/java/org/apache/sis/internal/referencing/Resources.java
@@ -29,6 +29,8 @@ import org.apache.sis.util.resources.ResourceInternationalString;
 
 /**
  * Warning and error messages that are specific to the {@code sis-referencing} module.
+ * Resources in this file should not be used by any other module. For resources shared
+ * by all modules in the Apache SIS project, see {@link org.apache.sis.util.resources} package.
  *
  * @author  Martin Desruisseaux (IRD, Geomatys)
  * @since   0.8
