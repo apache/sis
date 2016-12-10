737d3ede2ff4397473270d85e1882fc003067855
Martin Desruisseaux <desruisseaux@apache.org>
2016-12-10 17:24:03 +0000

    Add GeoTiffStoreProvider.
    git-svn-id: https://svn.apache.org/repos/asf/sis/branches/JDK8@1773526 13f79535-47bb-0310-9956-ffa450edef68

--- storage/sis-geotiff/src/main/java/org/apache/sis/internal/geotiff/Resources.java
+++ storage/sis-geotiff/src/main/java/org/apache/sis/internal/geotiff/Resources.java
@@ -129,8 +129,8 @@ public final class Resources extends IndexedResourceBundle {
         public static final short MissingValue_2 = 7;
 
         /**
-         * The file defines “{2}” with a value of {3}{4}, but that value should be {1}{4} for
-         * consistency with {0}.
+         * The file defines “{2}” with a value of {3}{4}, but that value should be {1}{4} according
+         * parent definition ({0}).
          */
         public static final short NotTheEpsgValue_5 = 17;
 
