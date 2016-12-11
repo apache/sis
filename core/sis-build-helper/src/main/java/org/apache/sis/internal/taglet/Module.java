bce8a71f68e58f816b9cc294ce6b141af0c6a0b2
Martin Desruisseaux <desruisseaux@apache.org>
2016-12-11 17:56:17 +0000

    Upgrade to the new doclet API. We have one unresolved issue, which is how to get the file where a tag is contained (was allowed in the old API, I didn't found the replacement yet).
    git-svn-id: https://svn.apache.org/repos/asf/sis/branches/JDK9@1773593 13f79535-47bb-0310-9956-ffa450edef68

--- core/sis-build-helper/src/main/java/org/apache/sis/internal/taglet/Module.java
+++ core/sis-build-helper/src/main/java/org/apache/sis/internal/taglet/Module.java
@@ -17,9 +17,12 @@
 package org.apache.sis.internal.taglet;
 
 import java.io.File;
-import java.util.Map;
-import com.sun.javadoc.Tag;
-import com.sun.tools.doclets.Taglet;
+import java.util.List;
+import java.util.Set;
+import java.util.EnumSet;
+import java.util.Collections;
+import jdk.javadoc.doclet.taglet.Taglet;
+import com.sun.source.doctree.DocTree;
 
 
 /**
@@ -27,36 +30,26 @@ import com.sun.tools.doclets.Taglet;
  *
  * @author  Martin Desruisseaux (IRD, Geomatys)
  * @since   0.3
- * @version 0.4
+ * @version 0.8
  * @module
  */
 public final class Module implements Taglet {
-    /**
-     * Register this taglet.
-     *
-     * @param tagletMap the map to register this tag to.
-     */
-    public static void register(final Map<String,Taglet> tagletMap) {
-       final Module tag = new Module();
-       tagletMap.put(tag.getName(), tag);
-    }
-
     /**
      * The SIS module in which the <code>@module</code> taglet has been found.
      */
     private String module;
 
     /**
-     * Constructs a default <code>@module</code> taglet.
+     * Constructs a <code>@module</code> taglet.
      */
-    private Module() {
+    public Module() {
         super();
     }
 
     /**
      * Returns the name of this custom tag.
      *
-     * @return The tag name.
+     * @return "module".
      */
     @Override
     public String getName() {
@@ -64,70 +57,20 @@ public final class Module implements Taglet {
     }
 
     /**
-     * Returns {@code false} since <code>@module</code> can not be used in overview.
+     * Returns the set of locations in which this taglet may be used.
      *
-     * @return Always {@code false}.
+     * @return the set of locations in which this taglet may be used.
      */
     @Override
-    public boolean inOverview() {
-        return false;
-    }
-
-    /**
-     * Returns {@code true} since <code>@module</code> can be used in package documentation.
-     *
-     * @return Always {@code true}.
-     */
-    @Override
-    public boolean inPackage() {
-        return true;
-    }
-
-    /**
-     * Returns {@code true} since <code>@module</code> can be used in type documentation
-     * (classes or interfaces). This is actually its main target.
-     *
-     * @return Always {@code true}.
-     */
-    @Override
-    public boolean inType() {
-        return true;
-    }
-
-    /**
-     * Returns {@code false} since <code>@module</code> can not be used in constructor.
-     *
-     * @return Always {@code false}.
-     */
-    @Override
-    public boolean inConstructor() {
-        return false;
-    }
-
-    /**
-     * Returns {@code false} since <code>@module</code> can not be used in method documentation.
-     *
-     * @return Always {@code false}.
-     */
-    @Override
-    public boolean inMethod() {
-        return false;
-    }
-
-    /**
-     * Returns {@code false} since <code>@module</code> can not be used in field documentation.
-     *
-     * @return Always {@code false}.
-     */
-    @Override
-    public boolean inField() {
-        return false;
+    public Set<Taglet.Location> getAllowedLocations() {
+        return EnumSet.of(Taglet.Location.PACKAGE,
+                          Taglet.Location.TYPE);
     }
 
     /**
      * Returns {@code false} since <code>@module</code> is not an inline tag.
      *
-     * @return Always {@code false}.
+     * @return always {@code false}.
      */
     @Override
     public boolean isInlineTag() {
@@ -135,33 +78,32 @@ public final class Module implements Taglet {
     }
 
     /**
-     * Given the <code>Tag</code> representation of this custom tag, return its string representation.
-     * The default implementation invokes the array variant of this method.
+     * Given the <code>DocTree</code> representation of this custom tag, return its string representation.
+     * The default implementation invokes the list variant of this method.
      *
-     * @param tag The tag to format.
-     * @return A string representation of the given tag.
+     * @param  tag  the tag to format.
+     * @return a string representation of the given tag.
      */
     @Override
-    public String toString(final Tag tag) {
-        return toString(new Tag[] {tag});
+    public String toString(final DocTree tag) {
+        return toString(Collections.singletonList(tag));
     }
 
     /**
-     * Given an array of {@code Tag}s representing this custom tag, return its string
-     * representation.
+     * Given a list of {@code DocTree}s representing this custom tag, returns its string representation.
      *
-     * @param tags The tags to format.
-     * @return A string representation of the given tags.
+     * @param  tags  the tags to format.
+     * @return a string representation of the given tags.
      */
     @Override
-    public String toString(final Tag[] tags) {
-        if (tags == null || tags.length == 0) {
+    public String toString(final List<? extends DocTree> tags) {
+        if (tags == null || tags.isEmpty()) {
             return "";
         }
         final StringBuilder buffer = new StringBuilder(128);
         buffer.append("\n<p><font size=\"-1\">");
-        for (final Tag tag : tags) {
-            File file = tag.position().file();
+        for (final DocTree tag : tags) {
+            File file = InlineTaglet.file(tag);
             module = file.getName();
             while ((file = file.getParentFile()) != null) {
                 if (file.getName().equals("src")) {
