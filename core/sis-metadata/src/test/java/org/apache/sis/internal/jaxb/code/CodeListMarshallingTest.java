414c94823c66992d01d8ab87ffd8dc16e94ddd5f
Martin Desruisseaux <desruisseaux@apache.org>
2018-01-08 04:12:36 +0000

    Skip commit e212fa862cd16c969cb2cd3c726a81ed8fef725d (XSLT experiment) Adaptation of commit 3d3bb1b0e47fcd6b4985943a4f33e307dcc59ab6 from Image Matter (got the first ISO 19115-3 round trip test working). We omit the ISOTestUtils class - instead we define the expected XML directly in the test.
    The project at this revision can be built but is known to have test failures.
    Tests will pass only after we completed the port of all ISO 19115-3 work.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/sis/branches/ISO-19115-3@1820526 13f79535-47bb-0310-9956-ffa450edef68

--- core/sis-metadata/src/test/java/org/apache/sis/internal/jaxb/code/CodeListMarshallingTest.java
+++ core/sis-metadata/src/test/java/org/apache/sis/internal/jaxb/code/CodeListMarshallingTest.java
@@ -32,6 +32,7 @@ import org.apache.sis.internal.jaxb.Schemas;
 import org.apache.sis.xml.XML;
 import org.apache.sis.xml.Namespaces;
 import org.apache.sis.xml.MarshallerPool;
+import org.apache.sis.test.DependsOnMethod;
 import org.apache.sis.test.XMLTestCase;
 import org.junit.Test;
 
@@ -53,11 +54,12 @@ import static org.apache.sis.test.Assert.*;
 public final strictfp class CodeListMarshallingTest extends XMLTestCase {
     /**
      * Returns a XML string to use for testing purpose.
+     * Note that responsible party exists only in legacy ISO 19115:2003 model.
      *
      * @param baseURL  the base URL of XML schemas.
      */
     private static String getResponsiblePartyXML(final String baseURL) {
-        return "<gmd:CI_ResponsibleParty xmlns:gmd=\"" + Namespaces.GMD + "\">\n" +
+        return "<gmd:CI_ResponsibleParty xmlns:gmd=\"" + LegacyNamespaces.GMD + "\">\n" +
                "  <gmd:role>\n" +
                "    <gmd:CI_RoleCode codeList=\"" + baseURL + Schemas.CODELISTS_PATH_LEGACY + "#CI_RoleCode\"" +
                     " codeListValue=\"principalInvestigator\">" + "Principal investigator</gmd:CI_RoleCode>\n" +
@@ -68,15 +70,31 @@ public final strictfp class CodeListMarshallingTest extends XMLTestCase {
     /**
      * Returns a XML string to use for testing purpose.
      *
-     * @param baseURL  the base URL of XML schemas.
+     * @param  language  three-letter ISO code.
+     * @param  dateType  date type code list in the language identified by {@code language}.
+     * @param  legacy    {@code true} for ISO 19139:2007 format, {@code false} for ISO 19115-3 format.
      */
-    private static String getCitationXML(final String baseURL, final String language, final String value) {
-        return "<gmd:CI_Date xmlns:gmd=\"" + Namespaces.GMD + "\">\n" +
-               "  <gmd:dateType>\n" +
-               "    <gmd:CI_DateTypeCode codeList=\"" + baseURL + Schemas.CODELISTS_PATH_LEGACY + "#CI_DateTypeCode\"" +
-                    " codeListValue=\"creation\" codeSpace=\"" + language + "\">" + value + "</gmd:CI_DateTypeCode>\n" +
-               "  </gmd:dateType>\n" +
-               "</gmd:CI_Date>";
+    private static String getCitationXML(final String language, final String dateType, final boolean legacy) {
+        final Object[] args = new Object[] {
+            "cit",                          // Prefix
+            Namespaces.CIT,                 // Namespace
+            Schemas.METADATA_ROOT,          // Base URL of code list path
+            Schemas.CODELISTS_PATH,         // Relative code list path in base URL
+            language, dateType
+        };
+        if (legacy) {
+            args[0] = "gmd";                              // Prefix
+            args[1] = LegacyNamespaces.GMD;               // Namespace
+            args[2] = Schemas.METADATA_ROOT_LEGACY;       // Base URL of code list path
+            args[3] = Schemas.CODELISTS_PATH_LEGACY;      // Relative code list path in base URL
+        }
+        return String.format(
+                "<%1$s:CI_Date xmlns:%1$s=\"%2$s\">\n" +
+                "  <%1$s:dateType>\n" +
+                "    <%1$s:CI_DateTypeCode codeList=\"%3$s%4$s#CI_DateTypeCode\"" +
+                     " codeListValue=\"creation\" codeSpace=\"%5$s\">%6$s</%1$s:CI_DateTypeCode>\n" +
+                "  </%1$s:dateType>\n" +
+                "</%1$s:CI_Date>", args);
     }
 
     /**
@@ -125,14 +143,36 @@ public final strictfp class CodeListMarshallingTest extends XMLTestCase {
      */
     @Test
     public void testLocalization() throws JAXBException {
+        testLocalization(false);
+    }
+
+    /**
+     * Tests a code list localization in ISO 19139:2007.
+     *
+     * @throws JAXBException if an error occurred while marshaling the XML.
+     *
+     * @since 1.0
+     */
+    @Test
+    @DependsOnMethod("testLocalization")
+    public void testLocalizationLegacyXML() throws JAXBException {
+        testLocalization(true);
+    }
+
+    /**
+     * Implementation of {@link #testLocalization()} and {@link #testLocalizationLegacyXML()}.
+     */
+    private void testLocalization(final boolean legacy) throws JAXBException {
         final MarshallerPool pool = getMarshallerPool();
         final Marshaller marshaller = pool.acquireMarshaller();
-        marshaller.setProperty(XML.METADATA_VERSION, LegacyNamespaces.ISO_19139);
+        if (legacy) {
+            marshaller.setProperty(XML.METADATA_VERSION, LegacyNamespaces.ISO_19139);
+        }
         /*
          * First, test using the French locale.
          */
         marshaller.setProperty(XML.LOCALE, Locale.FRENCH);
-        String expected = getCitationXML(Schemas.METADATA_ROOT_LEGACY, "fra", "Création");
+        String expected = getCitationXML("fra", "Création", legacy);
         CitationDate ci = (CitationDate) XML.unmarshal(expected);
         assertEquals(DateType.CREATION, ci.getDateType());
         String actual = marshal(marshaller, ci);
@@ -141,7 +181,7 @@ public final strictfp class CodeListMarshallingTest extends XMLTestCase {
          * Tests again using the English locale.
          */
         marshaller.setProperty(XML.LOCALE, Locale.ENGLISH);
-        expected = getCitationXML(Schemas.METADATA_ROOT_LEGACY, "eng", "Creation");
+        expected = getCitationXML("eng", "Creation", legacy);
         ci = (CitationDate) XML.unmarshal(expected);
         assertEquals(DateType.CREATION, ci.getDateType());
         actual = marshal(marshaller, ci);
@@ -159,21 +199,22 @@ public final strictfp class CodeListMarshallingTest extends XMLTestCase {
     public void testExtraCodes() throws JAXBException {
         final DefaultCitation id = new DefaultCitation();
         id.setPresentationForms(Arrays.asList(
-                PresentationForm.valueOf("IMAGE_DIGITAL"), // Existing code with UML id="imageDigital"
-                PresentationForm.valueOf("test")));        // New code
+                PresentationForm.valueOf("IMAGE_DIGITAL"),      // Existing code with UML id="imageDigital"
+                PresentationForm.valueOf("test")));             // New code
 
-        final String xml = marshal(id, LegacyNamespaces.ISO_19139);
-
-        // "IMAGE_DIGITAL" is marshalled as "imageDigital" because is contains a UML id, which is lower-case.
+        final String xml = marshal(id);
+        /*
+         * "IMAGE_DIGITAL" is marshalled as "imageDigital" because is contains a UML id, which is lower-case.
+         */
         assertXmlEquals(
-                "<gmd:CI_Citation xmlns:gmd=\"" + Namespaces.GMD + "\">\n" +
-                "  <gmd:presentationForm>\n" +
-                "    <gmd:CI_PresentationFormCode codeListValue=\"imageDigital\">Image digital</gmd:CI_PresentationFormCode>\n" +
-                "  </gmd:presentationForm>\n" +
-                "  <gmd:presentationForm>\n" +
-                "    <gmd:CI_PresentationFormCode codeListValue=\"test\">Test</gmd:CI_PresentationFormCode>\n" +
-                "  </gmd:presentationForm>\n" +
-                "</gmd:CI_Citation>\n",
+                "<cit:CI_Citation xmlns:cit=\"" + Namespaces.CIT + "\">\n" +
+                "  <cit:presentationForm>\n" +
+                "    <cit:CI_PresentationFormCode codeListValue=\"imageDigital\">Image digital</cit:CI_PresentationFormCode>\n" +
+                "  </cit:presentationForm>\n" +
+                "  <cit:presentationForm>\n" +
+                "    <cit:CI_PresentationFormCode codeListValue=\"test\">Test</cit:CI_PresentationFormCode>\n" +
+                "  </cit:presentationForm>\n" +
+                "</cit:CI_Citation>\n",
                 xml, "xmlns:*", "codeList", "codeSpace");
     }
 }
