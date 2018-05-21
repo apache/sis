6cf85aec37a98f7fef8f6149f30be463a2740060
Martin Desruisseaux <desruisseaux@apache.org>
2018-05-21 17:00:33 +0000

    Update Java requirements in the documentation (from Java 7 to Java 8).
    git-svn-id: https://svn.apache.org/repos/asf/sis/branches/JDK8@1831978 13f79535-47bb-0310-9956-ffa450edef68

--- core/sis-utility/src/main/java/org/apache/sis/internal/util/Numerics.java
+++ core/sis-utility/src/main/java/org/apache/sis/internal/util/Numerics.java
@@ -38,7 +38,7 @@ import static java.lang.Math.abs;
  */
 public final class Numerics extends Static {
     /**
-     * Some frequently used {@link Double} values. As of Java 7, those values do not
+     * Some frequently used {@link Double} values. As of Java 8, those values do not
      * seem to be cached by {@link Double#valueOf(double)} like JDK does for integers.
      */
     private static final Map<Object,Object> CACHE = new HashMap<>(32);
