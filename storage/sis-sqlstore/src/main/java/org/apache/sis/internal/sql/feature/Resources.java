ab5b6939a441a71343843a93c0063b18a32257de
Martin Desruisseaux <martin.desruisseaux@geomatys.com>
2018-07-10 18:35:38 +0200

    Rename "sis-sql" module as "sis-sqlstore" for consistency with "sis-xmlstore" and for avoiding confusion with an other module that may possibly be added later for putting a SQL front arbitrary DataStore.

--- storage/sis-sqlstore/src/main/java/org/apache/sis/internal/sql/feature/Resources.java
+++ storage/sis-sqlstore/src/main/java/org/apache/sis/internal/sql/feature/Resources.java
@@ -27,7 +27,7 @@ import org.apache.sis.util.resources.ResourceInternationalString;
 
 
 /**
- * Warning and error messages that are specific to the {@code sis-sql} module.
+ * Warning and error messages that are specific to the {@code sis-sqlstore} module.
  * Resources in this file should not be used by any other module. For resources shared by
  * all modules in the Apache SIS project, see {@link org.apache.sis.util.resources} package.
  *
