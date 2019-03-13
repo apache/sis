a960a63b3e460e441783f3e062d0d3651992bbc2
Martin Desruisseaux <martin.desruisseaux@geomatys.com>
2019-03-13 20:02:11 +0100

    Add the missing Apache license header. Pixel iterator implementations should be package-private.

--- core/sis-raster/src/main/java/org/apache/sis/image/LinearIterator.java
+++ core/sis-raster/src/main/java/org/apache/sis/image/LinearIterator.java
@@ -1,7 +1,18 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.sis.image;
 
@@ -11,8 +22,9 @@ import java.awt.image.Raster;
 import java.awt.image.RenderedImage;
 import java.awt.image.WritableRaster;
 import java.awt.image.WritableRenderedImage;
-import org.apache.sis.internal.raster.Resources;
 import org.opengis.coverage.grid.SequenceType;
+import org.apache.sis.internal.raster.Resources;
+
 
 /**
  * Linear iterator used when common line y line iteration is requiered.
@@ -28,15 +40,14 @@ import org.opengis.coverage.grid.SequenceType;
  * @version 1.0
  * @since   1.0
  * @module
- *
  */
-public class LinearIterator extends DefaultIterator {
+final class LinearIterator extends DefaultIterator {
 
-    public LinearIterator(Raster input, WritableRaster output, Rectangle subArea, Dimension window) {
+    LinearIterator(Raster input, WritableRaster output, Rectangle subArea, Dimension window) {
         super(input, output, subArea, window);
     }
 
-    public LinearIterator(final RenderedImage input, final WritableRenderedImage output, final Rectangle subArea, final Dimension window) {
+    LinearIterator(final RenderedImage input, final WritableRenderedImage output, final Rectangle subArea, final Dimension window) {
         super(input, output, subArea, window);
     }
 
