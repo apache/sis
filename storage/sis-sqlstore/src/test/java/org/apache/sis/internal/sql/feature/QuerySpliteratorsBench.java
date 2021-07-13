dda5ef6b7570eefd68c357d53fe489a8ed5f9ae7
Martin Desruisseaux <martin.desruisseaux@geomatys.com>
2021-07-13 15:24:54 +0200

    Document `QuerySpliteratorsBench`, use some services provide by testing framework and adjust build configuration.

--- storage/sis-sqlstore/src/test/java/org/apache/sis/internal/sql/feature/QuerySpliteratorsBench.java
+++ storage/sis-sqlstore/src/test/java/org/apache/sis/internal/sql/feature/QuerySpliteratorsBench.java
@@ -16,15 +16,16 @@
  */
 package org.apache.sis.internal.sql.feature;
 
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.Random;
-import java.util.concurrent.TimeUnit;
 
-import org.apache.derby.jdbc.EmbeddedDataSource;
-import org.apache.sis.internal.metadata.sql.LocalDataSource;
+import org.apache.sis.test.sql.TestDatabase;
+import org.apache.sis.util.ArgumentChecks;
 
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Fork;
@@ -37,53 +38,88 @@ import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.TearDown;
 import org.openjdk.jmh.annotations.Warmup;
 
-import static org.apache.sis.util.ArgumentChecks.ensureStrictlyPositive;
+import static org.junit.Assert.assertEquals;
+
 
 /**
+ * Measure the performance of the stream returned by {@link QueryFeatureSet#features(boolean)}.
  *
- * @author Alexis Manin (Geomatys)
- * @version 2.0
- * @since   2.0
+ * @author  Alexis Manin (Geomatys)
+ * @version 1.1
+ * @since   1.1
  * @module
  */
-@Fork(value = 2, jvmArgs = {"-server", "-Xmx2g"} )
+@Fork(value = 2, jvmArgs = {"-server", "-Xmx2G"} )
 @Warmup(iterations = 2, time = 4, timeUnit = TimeUnit.SECONDS)
 @Measurement(iterations = 2, time = 4, timeUnit = TimeUnit.SECONDS)
 public class QuerySpliteratorsBench {
-
+    /**
+     * Provide {@link QueryFeatureSet} instances to be tested.
+     * Various configurations are used (number of rows, parallelism, <i>etc.</i>).
+     *
+     * @see QuerySpliteratorsBench#test(DatabaseInput)
+     */
     @State(Scope.Benchmark)
     public static class DatabaseInput {
-
+        /**
+         * Number of rows to insert in the test database.
+         */
         @Param({"1000", "100000"})
         int numRows;
 
+        /**
+         * Argument to be given in the call to {@link QueryFeatureSet#features(boolean)}.
+         */
         @Param({"true", "false"})
         boolean parallel;
 
+        /**
+         * Value to assign to {@link QueryFeatureSet#allowBatchLoading}.
+         */
         @Param({"true", "false"})
-        boolean prefetch;
+        private boolean prefetch;
 
+        /**
+         * Value to assign to {@link QueryFeatureSet#fetchSize}.
+         */
         @Param({"10", "100", "1000"})
-        int fetchSize;
+        private int fetchSize;
 
+        /**
+         * Value to assign to {@link QueryFeatureSet#fetchRatio}.
+         */
         @Param({"0.5", "1", "2"})
-        float fetchRatio;
+        private float fetchRatio;
 
-        EmbeddedDataSource db;
+        /**
+         * Encapsulate a data source for an in-memory database.
+         */
+        private TestDatabase db;
+
+        /**
+         * The feature set to benchmark.
+         */
         QueryFeatureSet fs;
 
-        public DatabaseInput() {}
+        /**
+         * Creates a new set of {@link QueryFeatureSet} provider.
+         */
+        public DatabaseInput() {
+        }
 
+        /**
+         * Prepares a database populated with arbitrary rows, then creates a {@link QueryFeatureSet}
+         * for querying that database. The {@code QueryFeatureSet} configuration will vary between
+         * different calls to this method, depending on values injected by JMH in the annotated fields.
+         *
+         * @throws SQLException if an error occurred while preparing the database.
+         */
         @Setup(Level.Trial)
         public void setup() throws SQLException {
-            ensureStrictlyPositive("Number of rows", numRows);
+            ArgumentChecks.ensureStrictlyPositive("Number of rows", numRows);
 
-            db = new EmbeddedDataSource();
-            db.setDatabaseName("memory:spliterators");
-            db.setDataSourceName("Apache SIS test database");
-            db.setCreateDatabase("create");
-
-            try (Connection c = db.getConnection()) {
+            db = TestDatabase.create("spliterators");
+            try (Connection c = db.source.getConnection()) {
                 c.createStatement().execute(
                         "CREATE TABLE TEST (str CHARACTER VARYING(20), myInt INTEGER, myDouble DOUBLE)"
                 );
@@ -104,34 +140,43 @@ public class QuerySpliteratorsBench {
                     st.clearBatch();
                 } while (rows < numRows);
 
-                fs = new QueryFeatureSet("SELECT * FROM TEST", db, c);
+                fs = new QueryFeatureSet("SELECT * FROM TEST", db.source, c);
                 fs.allowBatchLoading = prefetch;
-                fs.fetchSize = fetchSize;
+                fs.fetchSize  = fetchSize;
                 fs.fetchRatio = fetchRatio;
             }
         }
 
+        /**
+         * Destroy the in-memory database.
+         *
+         * @throws SQLException in an error occurred.
+         */
         @TearDown
         public void dropDatabase() throws SQLException {
-            db.setCreateDatabase("no");
-            db.setConnectionAttributes("drop=true");
-            try {
-                db.getConnection().close();
-            } catch (SQLException e) {                          // This is the expected exception.
-                if (!LocalDataSource.isSuccessfulShutdown(e)) {
-                    throw e;
-                }
-            }
+            db.close();
         }
     }
 
+    /**
+     * Invoked by JMH for executing the operation that we want to benchmark.
+     *
+     * @param  input  encapsulate the {@link QueryFeatureSet} instance to benchmark.
+     * @throws SQLException if an error occurred during the database query.
+     */
     @Benchmark
     public void test(DatabaseInput input) throws SQLException {
         final int sum = input.fs.features(input.parallel).mapToInt(f -> 1).sum();
-        if (sum != input.numRows) throw new AssertionError("..." + sum + "..." + "WTF ?!");
+        assertEquals(input.numRows, sum);
     }
 
-    public static void main(String... args) throws Exception {
+    /**
+     * Launch the benchmark.
+     *
+     * @param  args  JMH command line argument.
+     * @throws IOException if JMH can not proceed.
+     */
+    public static void main(final String[] args) throws IOException {
         org.openjdk.jmh.Main.main(args);
     }
 }
