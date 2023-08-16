08f37c2d72378ee5e3130852038546ece60e8e69
Martin Desruisseaux <martin.desruisseaux@geomatys.com>
2023-08-16 14:53:26 +0200

    Add NetBeans project configuration files as Ant Modular projects. This is for development in NetBeans, because NetBeans 18 does not yet understand well a Gradle project with Module Source Hierarchy.

---
+++ netbeans-project/src-local/org.apache.sis.test.uncommitted/test/org/apache/sis/test/uncommitted/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * A dummy package for performing tests in a NetBeans environment.
+ * This package is <em>not</em> versioned. It is ignored by Git.
+ * This is a space available for local experiments.
+ */
+package org.apache.sis.test.uncommitted;
