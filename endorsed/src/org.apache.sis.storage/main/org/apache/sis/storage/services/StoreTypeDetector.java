9e1128778b48917b67e9b65ed23dc2a6f7c99c1e
Martin Desruisseaux <martin.desruisseaux@geomatys.com>
2023-12-14 18:02:26 +0100

    Move `StoreTypeDetector` to a dedicated "services" package. More service implementations may be added in the future.

--- endorsed/src/org.apache.sis.storage/main/org/apache/sis/storage/services/StoreTypeDetector.java
+++ endorsed/src/org.apache.sis.storage/main/org/apache/sis/storage/services/StoreTypeDetector.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.sis.storage.internal;
+package org.apache.sis.storage.services;
 
 import java.io.IOException;
 import java.nio.file.Path;
