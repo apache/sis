4a18444a15646b49fb849f7a95b0416e801a9462
Martin Desruisseaux <martin.desruisseaux@geomatys.com>
2025-10-01 18:43:50 +0200

    Set version number to 1.6-SNAPSHOT.

--- endorsed/src/org.apache.sis.openoffice/bundle/README.md
+++ endorsed/src/org.apache.sis.openoffice/bundle/README.md
@@ -83,7 +83,7 @@ Launch:
 
 ```
 cd target
-unopkg add apache-sis-1.5-SNAPSHOT.oxt --log-file log.txt
+unopkg add apache-sis-1.6-SNAPSHOT.oxt --log-file log.txt
 scalc -env:RTL_LOGFILE=log.txt
 ```
 
--- endorsed/src/org.apache.sis.util/main/org/apache/sis/system/Modules.java
+++ endorsed/src/org.apache.sis.util/main/org/apache/sis/system/Modules.java
@@ -116,7 +116,7 @@ public final class Modules {
      *
      * @see org.apache.sis.util.Version
      */
-    public static final int MINOR_VERSION = 5;
+    public static final int MINOR_VERSION = 6;
 
     /**
      * The prefix of all classnames in Apache SIS, including a trailing dot.
